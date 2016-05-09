225a647f64cfd5ea4488f639300490ac535c6b8b
Rajiv Aggarwal <raggarwal4@sapient.com>
2016-05-09 11:40:42 +0530

    Story SW - 450 Updated

--- src/components/article_list/js/article-list.js
+++ src/components/article_list/js/article-list.js
@@ -14,51 +14,101 @@ var INFORMA = window.INFORMA || {};
 INFORMA.article_list = (function(window, $, namespace) {
     'use strict';
     //variables
-    var _articleList = $('.article_list .list-container'),
-        _listItemCounts = _articleList.find("li").size(),
+    var el = $('.article-list .list-container'),
+        _listItemCounts = el.find("li").size(),
+        numberItemShown = (el.data("slides-show") !== null && el.data("slides-show") !== undefined) ? Math.floor(el.data("slides-show")) : 3,
         // methods
         init,
-        _createSlider;
+        _createSlider, _getDataOptions,
+        partial = {
+            'match': 
+                '{{#each articleListItems}}'+
+                        '<li>'+
+                            '<p class="category">{{ContentType}}'+
+                              '<strong>{{SectorType}}</strong>'+
+                            '</p>'+
+                            '<img src="{{Image}}" alt="{{ImageAltText}}" />'+
+                            '<h2 class="poduct-brand-subheading">{{Title}}</h2>'+
+                            '<p class="date">30.03.2016</p>'+
+                            '<div class="list-content">'+
+                                '{{#if Description}}'+
+                                    '<p class="description">{{Description}}</p>'+
+                                    '<span class="article-info"><em>Author:</em> <strong>{{Profiles}}</strong></span>'+
+                                        '<span class="article-info"><em>Topic:</em>'+ 
+                                            '<strong>{{Topics}}</strong>'+
+                                        '</span>'+
+                                '{{/if}}'+
+                                '{{#if Video}}'+
+                                    '<div class="video-container">'+
+                                        '<a href="{{Video.url}}" class="video-link">'+
+                                            '<img src="{{Video.ImageSrc}}" alt="{{Video.ImageAltText}}" />'+
+                                        '</a>'+
+                                        '<span class="play-icon"></span>'+
+                                    '</div>'+
+                                '{{/if}}'+
+                            '</div>'+
+                            '{{#if ResourceLink}}'+
+                                '<div class="btn-container">'+
+                                  '<a role="button" href="{{ResourceLink.url}}" class="btn btn-default" target="{{ResourceLink.target}}">{{ResourceLink.linkText}}</a>'+
+                                '</div>'+
+                            '{{/if}}'+
+                        '</li>'+
+                      '{{/each}}'
+            
+        };
 
-    _createSlider = function() {
-        // if data-items, data-infinite is defined, used it
-        _articleList.slick({
-            dots: true,
-            infinite: true,
-            speed: 400,
-            autoplay: true,
-            autoplaySpeed:4000,
-            slidesToShow: (_listItemCounts >= 3 ) ? 3 : _listItemCounts,
-            slidesToScroll: 3,
-            responsive: [{ //TODO - SW450 - Setting need to be move to config file.
-                    breakpoint: 1024,
-                    settings: {
-                        slidesToShow: (_listItemCounts >= 2 ) ? 2 : _listItemCounts,
-                        slidesToScroll: 2,
-                        dots: true
+    //get all default setting value from component and check 
+    //if exist any default setting then return true else false.
+
+    _getDataOptions = function(key) {
+            var dataValue = el.data(key);
+            if (dataValue !== null && dataValue !== undefined) {
+                return true;
+            }
+            return false;
+        },
+        _createSlider = function() {
+
+            // if data-items, data-infinite is defined, used it  - Todo - SW-450 - move null and undefined check to method to remove repetition
+            el.slick({
+                dots: (_getDataOptions("slider-dots")) ? el.data("slider-dots") : false,
+                infinite: (_getDataOptions("infinite")) ? el.data("infinite") : true,
+                speed: (_getDataOptions("speed")) ? el.data("speed") : 400,
+                autoplay: (_getDataOptions("autoplay")) ? el.data("autoplay") : true,
+                autoplaySpeed: (_getDataOptions("autoplay-speed")) ? el.data("autoplay-speed") : 4000,
+                slidesToShow: (_listItemCounts >= numberItemShown) ? numberItemShown : _listItemCounts,
+                slidesToScroll: (_getDataOptions("slides-scroll")) ? Math.floor(el.data("slides-scroll")) : 3,
+                responsive: [{ //TODO - SW450 - Setting need to be move to config file.
+                        breakpoint: 1024,
+                        settings: {
+                            slidesToShow: (_listItemCounts >= 2) ? 2 : _listItemCounts,
+                            slidesToScroll: 2
+                        }
+                    }, {
+                        breakpoint: 600,
+                        settings: {
+                            slidesToShow: (_listItemCounts >= 2) ? 2 : _listItemCounts,
+                            slidesToScroll: 2
+                        }
+                    }, {
+                        breakpoint: 480,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1
+                        }
                     }
-                }, {
-                    breakpoint: 600,
-                    settings: {
-                        slidesToShow: (_listItemCounts >= 2 ) ? 2 : _listItemCounts,
-                        slidesToScroll: 2
-                    }
-                }, {
-                    breakpoint: 480,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1
-                    }
-                }
-                // You can unslick at a given breakpoint now by adding:
-                // settings: "unslick"
-                // instead of a settings object
-            ]
-        });
-    }
+                    // You can unslick at a given breakpoint now by adding:
+                    // settings: "unslick"
+                    // instead of a settings object
+                ]
+            });
+
+            //var template = Handlebars.compile(partial.match),
+            //myhtml = template({ articleListItems : mytest.articleListItems });
+        }
 
     init = function() {
-        if (_articleList.length > 0) {
+        if (el.length > 0) {
             _createSlider();
         }
     };
