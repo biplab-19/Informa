cc9cd521c384b7fe6392c7161e7eac3a9e310f2b
Saurabh Sinha <ssi253@sapient.com>
2016-05-10 21:04:48 +0530

    no message

---
+++ src/components/hero-banner/js/hero-banner.js
@@ -0,0 +1,37 @@
+/*
+ * Hero Video.js
+ *
+ *
+ * @project:    Informa
+ * @date:       2016-April-25
+ * @author:     Saurabh Sinha
+ * @licensor:   SAPIENNITRO
+ * @namespaces: INFORMA
+ *
+ */
+
+var INFORMA = window.INFORMA || {};
+INFORMA.heroBanner = (function(window, $, namespace) {
+    'use strict';
+    //variables
+    var _videoElem = $('img[data-video]'),
+    // methods
+        init,
+        _bindIframe;
+
+    _bindIframe = function(){
+        var videoUrl = _videoElem.data('video');
+        _videoElem.parent().html('<iframe width="100%" height="100%" src="'+videoUrl+'" allowfullscreen volume="0"></iframe>');
+    };
+
+    init = function() {
+        if (_videoElem.length > 0) {
+           _bindIframe();
+        }
+    };
+
+    return {
+        init: init
+    };
+}(this, jQuery, 'INFORMA'));
+jQuery(INFORMA.heroBanner.init());
