28225ef4dbb1f2b4e4130a6b199d3125cbce67e9
Jagadeesh <jjayachandran2@sapient.com>
2016-05-10 20:42:12 +0530

    Added support for sitecore.experience/preview check

--- src/scripts/global.js
+++ src/scripts/global.js
@@ -15,32 +15,42 @@ INFORMA.global = (function(window, $, namespace) {
 	'use strict';
 	//variables
 	var device = {},
-        _html = $('html');
+		siteCore = {},
+		_html = $('html');
 
-    var init = function(){
-        // viewport properties
-        var _viewportWidth = $(window).width();
-        if(_viewportWidth >= 1142){
-          device.isDesktop = true;
-          device.viewport = 'desktop';
-		  device.viewportN = 0;
-        }
-        else if(_viewportWidth >= 768){
-          device.isTablet = true;
-          device.viewport = 'tablet';
-		  device.viewportN = 1;
-        }
-        else {
-          device.isMobile = true;
-          device.viewport = 'mobile';
-		  device.viewportN = 2;
-        }
-        _html.addClass(device.viewport);
-    }
+	var init = function(){
+		// viewport properties
+		var _viewportWidth = $(window).width();
+		if(_viewportWidth >= 1142){
+			device.isDesktop = true;
+			device.viewport = 'desktop';
+			device.viewportN = 0;
+		}
+		else if(_viewportWidth >= 768){
+			device.isTablet = true;
+			device.viewport = 'tablet';
+			device.viewportN = 1;
+		}
+		else {
+			device.isMobile = true;
+			device.viewport = 'mobile';
+			device.viewportN = 2;
+		}
+		_html.addClass(device.viewport);
+
+		// siteCore properties
+		if( $('html').hasClass('preview-mode')){
+			siteCore.isPreview = true;
+		}
+		else if($('html').hasClass('experience-mode')){
+			siteCore.isExperience = true;
+		}
+	}
 
 	return {
-        init: init,
-		device: device
+		init: init,
+		device: device,
+		siteCore: siteCore
 	};
-}(this, jQuery, 'INFORMA'));
+}(this, $INFORMA = jQuery.noConflict(), 'INFORMA'));
 jQuery(INFORMA.global.init());
