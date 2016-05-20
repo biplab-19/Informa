c30775fea6bd95f70def9560adb0f6a8bff10f2b
Tejaswi Chennupati <tchen3@sapient.com>
2016-05-20 13:35:48 +0530

    Trainig material + Hero banner

---
+++ src/components/training-material/js/training-material.js
@@ -0,0 +1,62 @@
+/*
+ * training-material.js
+ *
+ *
+ * @project:    Informa
+ * @date:       2016-May-18
+ * @author:     Tejaswi
+ * @licensor:   SAPIENNITRO
+ * @namespaces: INFORMA
+ *
+ */
+
+var INFORMA = window.INFORMA || {};
+INFORMA.trainingMaterial = (function(window, $, namespace) {
+    'use strict';
+    //variables
+    var _traininglist = $('.slick-carousel'),
+    // methods
+        init,
+        _dots,
+        _createSlider;
+
+    _createSlider = function(container){
+        // if data-items, data-infinite is defined, used it
+        var _slideCount = container.data('itemsperframe'),
+            _autoplay = container.data('autorotate'),
+            _speed = container.data('transitionspeed'), // speed of transition
+            _duration = container.data('slideduration'), // how long the slider will be displayed
+            _infinite = true,
+            _dots = Boolean(container.data('pagination'));
+            //chk for sitecore preview
+            if (INFORMA.global.siteCore.isPreview) {
+                _autoplay = true;
+            }
+            if (INFORMA.global.siteCore.isExperience) {
+                _autoplay = false;
+                _infinite = false;
+            }
+
+        container.slick({
+            infinite: _infinite,
+            autoplay: _autoplay,
+            autoplaySpeed: _duration,
+            slidesToShow: _slideCount,
+            slidesToScroll: _slideCount,
+            speed: _speed,
+            dots: _dots,
+            adaptiveHeight: true
+        });
+    }
+
+    init = function() {
+        if (_traininglist.length > 0) {
+            _createSlider(_traininglist);
+        }
+    };
+
+    return {
+        init: init
+    };
+}(this, $INFORMA = jQuery.noConflict(), 'INFORMA'));
+jQuery(INFORMA.trainingMaterial.init());
