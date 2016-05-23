d45abfebfa535d041512ef407c94c6a5403ecb8c
Santosh Ganthasetti <sganthasetti@sapient.com>
2016-05-23 17:35:10 +0530

    Build package fix: Artcle Headlines reference

--- dist/unminified/Static/js/components.js
+++ dist/unminified/Static/js/components.js
@@ -21,56 +21,6 @@
 }
 
 /*
- * analyst-list.js
- *
- *
- * @project:    Informa
- * @date:       2016-April-25
- * @author:     Saurabh Sinha
- * @licensor:   SAPIENNITRO
- * @namespaces: INFORMA
- *
- */
-
-var INFORMA = window.INFORMA || {};
-INFORMA.analystList = (function(window, $, namespace) {
-    'use strict';
-    //variables
-    var _analystList = $('#pdp-analyst'),
-    // methods
-        init,
-        _bindShowMore;
-
-    _bindShowMore = function(container){
-        
-        // if data-items, data-infinite is defined, used it
-        var _showMore = $('.btn-showMore');
-        _showMore.on('click',function(){
-            var _vp = INFORMA.global.device.viewportN;
-            if(_vp == 2) {// This is mobile, toggle everything except first twbs-font-path
-                _vp = 1; //to emulate nth-child(n+3)
-            }
-            else {
-                _vp = 4; // or (n+9)
-            }
-            _analystList.find('.analyst-list-container:nth-child(n+'+_vp+')').slideToggle();
-            $(this).toggleClass('showLess');
-        });
-    }
-
-    init = function() {
-        if (_analystList.length > 0) {
-            _bindShowMore(_analystList);
-        }
-    };
-
-    return {
-        init: init
-    };
-}(this, jQuery, 'INFORMA'));
-jQuery(INFORMA.analystList.init());
-
-/*
  * global-footer.js
  *
  *
@@ -199,7 +149,7 @@ INFORMA.ArticleList = (function(window, $, namespace) {
         },
         CreateSlider, GetCarouselOptions, GetCarouselUpdatedHtml, GetCarouselData, equalHeights, RenderCarousel, BindFilterEvents, GetListCount;
 
-    //get all default setting value from component and check 
+    //get all default setting value from component and check
     //if exist any default setting then update and return carousel object.
 
     GetCarouselOptions = function(ele) {
@@ -237,12 +187,12 @@ INFORMA.ArticleList = (function(window, $, namespace) {
                 data: data,
                 success_callback: function(data) {
                     if (data.Articles !== undefined && data.Articles.length > 0) {
-                        var html = GetCarouselUpdatedHtml(Templates.articleListItems, { Articles: data.Articles });
+                        var html = GetCarouselUpdatedHtml(INFORMA.Templates.articleListItems, { Articles: data.Articles });
                         _ArticleLists.slick('unslick');
                         RenderCarousel(html, _ArticleLists);
                     }
                     if (data.Articles !== undefined && data.Headlines.length > 0) {
-                        var html = GetCarouselUpdatedHtml(Templates.HeadlinesListItems, { Headlines: data.Headlines });
+                        var html = GetCarouselUpdatedHtml(INFORMA.Templates.HeadlinesListItems, { Headlines: data.Headlines });
                         _HeadlinesLists.slick('unslick');
                         RenderCarousel(html, _HeadlinesLists);
                     }
@@ -269,7 +219,7 @@ INFORMA.ArticleList = (function(window, $, namespace) {
 
                 });
 
-                // Set the height of all those children to whichever was highest 
+                // Set the height of all those children to whichever was highest
                 $('.columns', this).height(highestBox);
 
             });
@@ -437,6 +387,7 @@ INFORMA.featureList = (function(window, $, namespace) {
     };
 }(this, $INFORMA = jQuery.noConflict(), 'INFORMA'));
 jQuery(INFORMA.featureList.init());
+
 
 /*
  * global.js
@@ -901,11 +852,11 @@ INFORMA.analystList = (function(window, $, namespace) {
     'use strict';
     //variables
     var _analystList = $('#pdp-analyst'),
+        _listItems = $('.analyst-views'),
     // methods
         init,
         _bindShowMore,
-        _bindElement,
-        _template,
+        _equalHeight,
         _lists = null;
 
     _bindShowMore = function(container){
@@ -927,61 +878,32 @@ INFORMA.analystList = (function(window, $, namespace) {
         });
     }
 
-    _template = function(obj) {
-        _lists += '<div class="col-xs-12 col-sm-6 col-md-4 analyst-list-container">'+
-                    '<div class="meet-anlyst-section">'+
-                        '<div class="anlyst-heading">'+
-                            '<div class="analyst-details">'+
-                                '<h2>'+obj.Title+'</h2>'+
-                                '<h3>'+obj.Description+'</h3>'+
-                                '<p class="location">'+obj.Location+'</p>'+
-                            '</div>'+
-                            '<div class="analyst-img">'+
-                                '<img src="{{image}}" alt="{{image}}" />'+
-                            '</div>'+
-                        '</div>'+
-                        '<div class="analyst-description">'+
-                            '<p class="heading">{{question}}</p>'+
-                            '<ul class="yellow-bullets">'+
-                                // {{#each specialities}}
-                                //     <li>{{name}}</li>
-                                // {{/each}}
-                            '</ul>'+
-                            '<p class="heading">{{experience}}</p>'+
-                            '<ul class="track-analyst clearfix">'+
-                                // {{#each products}}
-                                //     <li><a href="#">{{name}}</a></li>
-                                // {{/each}}
-                            '</ul>'+
-                        '</div>'+
-                        '<div class="analyst-footer clearfix">'+
-                            '<ul class="nav-links">'+
-                                '<li><a href="#" class="white-twitter"></a></li>'+
-                                '<li><a href="#" class="white-linkedin"></a></li>'+
-                                '<li><a href="#" class="white-mail"></a></li>'+
-                            '</ul>'+
-                            '<a href="#" class="btn btn-default orange pull-right">Full Profile</a>'+
-                        '</div>'+
-                    '</div>'+
-                '</div>';
-    }
+    _equalHeight = function(items) {
+        var _analystDescription = items.find('.analyst-description'),
+            _docWidth = jQuery(document).width(),
+            _eachItemWidth = jQuery(items.find('.analyst-description')[0]).width(),
+            _maxHeight = 0;
+            _analystDescription.each(function() {
+                var _currentHeight = jQuery(this).height();
+                if(_currentHeight > _maxHeight) {
+                    _maxHeight = _currentHeight;
+                }
+            });
+            debugger;
+            _analystDescription.css('height',_maxHeight+50);
 
-    _bindElement = function () {
-        var self = this;
-        $.get('../json/analyst.json', function(data) {
-            var x = null;
-            for(x in data.ResultPayLoad) {
-                _template(data.ResultPayLoad[x]);
-            }
-
-            $('.analyst-items').html(_lists);
-        });
     }
 
     init = function() {
         if (_analystList.length > 0) {
            // _bindElement();
             _bindShowMore(_analystList);
+        }
+        if (_listItems.length > 0) {
+            _listItems.each(function() {
+                var items = jQuery(this).find('.analyst-list-container');
+                _equalHeight(items);
+            });
         }
     };
 
