c4c908099eea93c22cd4d5c1d6f0adc3d151b60d
Santosh Ganthasetti <sganthasetti@sapient.com>
2016-05-25 14:13:54 +0530

    Build Pakage: 25-05

--- dist/deploy/Static/js/main.js
+++ dist/deploy/Static/js/main.js
@@ -1,4 +1,4 @@
-/*! 2016-05-24 */var INFORMA = window.INFORMA || {};
+/*! 2016-05-25 */var INFORMA = window.INFORMA || {};
 (function(window, $, namespace) {
 	'use strict';
     INFORMA.getCookie =function(cname) {
@@ -339,10 +339,20 @@ var INFORMA = window.INFORMA || {};
   }
 }(this, jQuery, 'INFORMA'));
 
-(function (INFORMA, $) {
-     INFORMA.Utils = (function () {
+(function(INFORMA, $) {
+    INFORMA.Utils = (function() {
         function _utils() {
-            this.getIEVersion = function(){
+
+            this.getUniqueArray = function(arrayList) {
+                var uniqueArray = [];
+                $.each(arrayList, function(i, el) {
+                    if ($.inArray(el, uniqueArray) === -1) {
+                        uniqueArray.push(el);
+                    }
+                });
+                return uniqueArray;
+            }
+            this.getIEVersion = function() {
                 var agent = navigator.userAgent;
                 var reg = /MSIE\s?(\d+)(?:\.(\d+))?/i;
                 var matches = agent.match(reg);
@@ -352,59 +362,59 @@ var INFORMA = window.INFORMA || {};
                 return { major: '-1', minor: '-1' };
             }
             this.getViewport = function() {
-                var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-                var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+                    var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+                    var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 
-                var size;
-                if( $('html').hasClass('lt-ie9') ) {
-                    size = 'large';
-                } else {
-                    size = ( w <= INFORMA.Configs.views.xsmall )? 'xsmall' : size;
-                    size = ( w >  INFORMA.Configs.views.xsmall && w <= INFORMA.Configs.views.small  )? 'small'  : size;
-                    size = ( w >  INFORMA.Configs.views.small  )? 'medium' : size;
-                    size = ( w >  INFORMA.Configs.views.medium )? 'large'  : size;
-                    size = ( w >  INFORMA.Configs.views.large  )? 'xlarge' : size;
-                }
+                    var size;
+                    if ($('html').hasClass('lt-ie9')) {
+                        size = 'large';
+                    } else {
+                        size = (w <= INFORMA.Configs.views.xsmall) ? 'xsmall' : size;
+                        size = (w > INFORMA.Configs.views.xsmall && w <= INFORMA.Configs.views.small) ? 'small' : size;
+                        size = (w > INFORMA.Configs.views.small) ? 'medium' : size;
+                        size = (w > INFORMA.Configs.views.medium) ? 'large' : size;
+                        size = (w > INFORMA.Configs.views.large) ? 'xlarge' : size;
+                    }
 
-                INFORMA.Configs.viewport = {
-                    size: size,
-                    width: w,
-                    height: h
+                    INFORMA.Configs.viewport = {
+                        size: size,
+                        width: w,
+                        height: h
+                    };
+
+                    return INFORMA.Configs.viewport;
+                },
+                this.isMobileView = function() {
+                    return (this.getViewport().size == 'small' || this.getViewport().size == 'xsmall');
+                },
+                this.isTabletView = function() {
+                    return (this.getViewport().size == 'medium');
                 };
-
-                return INFORMA.Configs.viewport;
-            },
-            this.isMobileView = function() {
-                return (this.getViewport().size == 'small' || this.getViewport().size == 'xsmall');
-            },
-            this.isTabletView = function() {
-                return (this.getViewport().size == 'medium');
-            };
-			this.init = function () {
-				var that = this; //to behave proxy
-				this.getViewport();
-				//--------------------------------------------------
-				// Add IE10 Class
-				//--------------------------------------------------
-				if(this.getIEVersion().major === '10') {
-					$('html').addClass('ie10');
-				}
-				//--------------------------------------------------
-				//--------------------------------------------------
-				// RESIZE EVENT
-				// Fires "windowResize" on $(window)
+            this.init = function() {
+                var that = this; //to behave proxy
+                this.getViewport();
+                //--------------------------------------------------
+                // Add IE10 Class
+                //--------------------------------------------------
+                if (this.getIEVersion().major === '10') {
+                    $('html').addClass('ie10');
+                }
+                //--------------------------------------------------
+                //--------------------------------------------------
+                // RESIZE EVENT
+                // Fires "windowResize" on $(window)
                 //Custom resize on particular breakpoints
-				//--------------------------------------------------
+                //--------------------------------------------------
                 return this;
-			};
-		}
-		return new _utils();
-	 }());
-}(window.INFORMA = ( typeof INFORMA !== 'undefined' && INFORMA instanceof Object ) ? INFORMA : {}, $INFORMA));
+            };
+        }
+        return new _utils();
+    }());
+}(window.INFORMA = (typeof INFORMA !== 'undefined' && INFORMA instanceof Object) ? INFORMA : {}, $INFORMA));
 
-var logThis = function(throwLog){
-    if(INFORMA.Configs.debug){
-        for(var i = 0 ; i < arguments.length; i++){
+var logThis = function(throwLog) {
+    if (INFORMA.Configs.debug) {
+        for (var i = 0; i < arguments.length; i++) {
             console.log(arguments[i])
         }
     }
