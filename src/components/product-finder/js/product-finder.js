08f4078c4a91e3c1cf0e6519c2c9bb8b1599e904
Rajiv Aggarwal <raggarwal4@sapient.com>
2016-05-25 12:27:18 +0530

    SW 804

--- src/components/product-finder/js/product-finder.js
+++ src/components/product-finder/js/product-finder.js
@@ -3,8 +3,8 @@
  *
  *
  * @project:    Informa
- * @date:       2016-April-25
- * @author:     Saurabh Sinha
+ * @date:       2016-May-22
+ * @author:     Rajiv Aggarwal
  * @licensor:   SAPIENNITRO
  * @namespaces: INFORMA
  *
@@ -15,13 +15,75 @@ INFORMA.ProductFinder = (function(window, $, namespace) {
     'use strict';
     //variables
     var ProductFinderSection = $('#product-finder-section'),
-    // methods
-        init;
+        // methods
+        init, GetSubSectorList, ToggleSearchOption, BindDropDown,ShowHideSeach;
 
+        ShowHideSeach = function(ele) {
+            var ShowOption = $(ele).data('show');
+            $("ul.searchToggle").addClass('hidden');
+            ProductFinderSection.find("ul." + ShowOption).removeClass("hidden").fadeIn("slow");
+        },
+        ToggleSearchOption = function() {
+            jQuery(".search-options input[type=radio]").on('change', function(e) {
+                e.preventDefault();
+                ShowHideSeach($(this));
+            });
+            var CheckedOption = jQuery(".search-options input[type=radio]:checked");
+            if (typeof CheckedOption === "object") {
+                ShowHideSeach(CheckedOption);
+            }
+        },
+        GetSubSectorList = function(arrayList) {
 
+            var SectorData = {};
+            SectorData.sector = INFORMA.Utils.getUniqueArray(arrayList);
+            console.log(JSON.stringify(SectorData));
+            INFORMA.DataLoader.GetServiceData("/client/search/getsubsector", {
+                method: "GET",
+                data: JSON.stringify(SectorData),
+                success_callback: function(data) {
+
+                },
+                error_callback: function() {
+
+                }
+            });
+        },
+        BindDropDown = function() {
+            var SectorList = [];
+            jQuery(".custom-multiselect select").multiselect({
+                buttonText: function(o, s) {
+
+                    if (o.length === 0) {
+                        return $(s).data('placeholder');
+                    } else {
+                        var labels = 1;
+                        o.each(function(i) {
+                            labels = parseInt(1 + i);
+                        });
+                        return labels + ' Selected';
+                    }
+                },
+                onChange: function(option, checked, select) {
+                    if (checked) {
+                        SectorList.push($(option).val());
+                    } else {
+                        var index = SectorList.indexOf($(option).val());
+                        if (index >= 0) {
+                            SectorList.splice(index, 1);
+                        }
+                    }
+                    if (SectorList.length > 0) {
+                        GetSubSectorList(SectorList);
+                    }
+                }
+            });
+        };
     init = function() {
+
         if (ProductFinderSection.length > 0) {
-            jQuery(".custom-multiselect select").multiselect({ numberDisplayed: 0});
+            BindDropDown();
+            ToggleSearchOption();
         }
     };
 
