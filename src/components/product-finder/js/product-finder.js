dbfb7c1d101d32b37e42978ed14884150ab679df
Rajiv Aggarwal <raggarwal4@sapient.com>
2016-05-26 17:21:00 +0530

    SW-804 Loader added

--- src/components/product-finder/js/product-finder.js
+++ src/components/product-finder/js/product-finder.js
@@ -66,6 +66,10 @@ INFORMA.ProductFinder = (function(window, $, namespace) {
 
             var SectorData = {};
             SectorData.SectorIDs = INFORMA.Utils.getUniqueArray(arrayList);
+
+            //Show spinner in sub sector list
+            INFORMA.Spinner.Show(SubSectorList.parent());
+
             INFORMA.DataLoader.GetServiceData(Urls.GetSubSectorList, {
                 method: "GET",
                 data: JSON.stringify(SectorData),
