0b03f0a1660f30924cda795ae560063278bc5a2f
Jagadeesh <jjayachandran2@sapient.com>
2016-05-27 18:38:11 +0530

    Adding interaction support for Tooltip

--- src/components/form-request-demo/js/form-request-demo.js
+++ src/components/form-request-demo/js/form-request-demo.js
@@ -13,7 +13,11 @@
 var INFORMA = window.INFORMA || {};
 INFORMA.formRequestForDemo = (function(window, $, namespace) {
     'use strict';
-     var init,
+     var _toolTip = $('.hasToolTip .icon.icon-info'),
+
+//functions
+     init,
+      _bindToolTip,
         _showOverlay;
 
     _showOverlay = function(container){
@@ -22,10 +26,21 @@ INFORMA.formRequestForDemo = (function(window, $, namespace) {
     }
 
     init = function() {
+          //todo: No null check, dont execute these bindings if forms are not there
 
             _showOverlay();
+            _bindToolTip();
 
     };
+
+    _bindToolTip = function(){
+          _toolTip.on('click',function(){
+                $(this).toggleClass('active');
+                $(this).parent().parent() // .hasToolTip
+                        .children('.tooltip-placeholder').slideToggle();
+          })
+   }
+
 
     return {
         init: init
