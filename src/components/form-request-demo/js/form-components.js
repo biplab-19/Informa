19c250c5d46e761f3d8bb3fce7dd99e061750207
Santosh Ganthasetti <sganthasetti@sapient.com>
2016-05-31 18:03:37 +0530

    form components

---
+++ src/components/form-request-demo/js/form-components.js
@@ -0,0 +1,48 @@
+/*
+ * analyst-list.js
+ *
+ *
+ * @project:    Informa
+ * @date:       2016-April-25
+ * @author:     Saurabh Sinha
+ * @licensor:   SAPIENNITRO
+ * @namespaces: INFORMA
+ *
+ */
+
+var INFORMA = window.INFORMA || {};
+INFORMA.formComponents = (function(window, $, namespace) {
+    'use strict';
+     var _toolTip = $('.hasToolTip .icon.icon-info'),
+
+//functions
+     init,
+      _bindToolTip,
+        _showOverlay;
+
+    _showOverlay = function(container){
+
+      //alert(1);
+    }
+
+    init = function() {
+          //todo: No null check, dont execute these bindings if forms are not there
+          //  _showOverlay();
+          //  _bindToolTip();
+            $(".elq-form").validate();
+    };
+
+    _bindToolTip = function(){
+          _toolTip.on('click',function(){
+                $(this).toggleClass('active');
+                $(this).parent().parent() // .hasToolTip
+                        .children('.tooltip-placeholder').slideToggle();
+          })
+   }
+
+
+    return {
+        init: init
+    };
+}(this, jQuery, 'INFORMA'));
+jQuery(INFORMA.formComponents.init());
---
+++ src/components/form-request-demo/js/handlebar-helper.js
@@ -0,0 +1,49 @@
+// {{compare unicorns ponies operator="<"}}
+// 	I knew it, unicorns are just low-quality ponies!
+// {{/compare}}
+
+
+Handlebars.registerHelper('compare', function(lvalue, rvalue, options) {
+
+  if (arguments.length < 3)
+    throw new Error("Handlerbars Helper 'compare' needs 2 parameters");
+
+  var operator = options.hash.operator || "==";
+
+  var operators = {
+    '==':		function(l,r) { return l == r; },
+    '===':	function(l,r) { return l === r; },
+    '!=':		function(l,r) { return l != r; },
+    '<':		function(l,r) { return l < r; },
+    '>':		function(l,r) { return l > r; },
+    '<=':		function(l,r) { return l <= r; },
+    '>=':		function(l,r) { return l >= r; },
+    'typeof':	function(l,r) { return typeof l == r; }
+  }
+
+  if (!operators[operator])
+    throw new Error("Handlerbars Helper 'compare' doesn't know the operator "+operator);
+
+  var result = operators[operator](lvalue,rvalue);
+
+  if( result ) {
+    return options.fn(this);
+  } else {
+    return options.inverse(this);
+  }
+
+});
+
+
+Handlebars.registerHelper("math", function(lvalue, operator, rvalue, options) {
+    lvalue = parseFloat(lvalue);
+    rvalue = parseFloat(rvalue);
+
+    return {
+        "+": lvalue + rvalue,
+        "-": lvalue - rvalue,
+        "*": lvalue * rvalue,
+        "/": lvalue / rvalue,
+        "%": lvalue % rvalue
+    }[operator];
+});
