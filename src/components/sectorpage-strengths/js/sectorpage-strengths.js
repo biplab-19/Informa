7058370e7f08728a87127f5ce97497b540366bbe
Tejaswi Chennupati <tchen3@sapient.com>
2016-06-06 16:49:36 +0530

    ++sectorpage-strengths

--- src/components/sectorpage-strengths/js/sectorpage-strengths.js
+++ src/components/sectorpage-strengths/js/sectorpage-strengths.js
@@ -14,24 +14,54 @@ var INFORMA = window.INFORMA || {};
 INFORMA.sectorPageStrengths = (function(window, $, namespace) {
     'use strict';
     //variables
+
     var _sectorPageStrengths = $('.sectorpage-strengths'),
+        _elements = 0,
     // methods
         init,
         _bindShowMore,
-        _adjustHeigt;
+        _adjustHeigt, _checkElemnt;
+
+
+    _checkElemnt = function () {
+        var _vp = INFORMA.global.device.viewportN;
+
+        if(_vp == 0) {
+            var count = _sectorPageStrengths.data('desktop');
+            _sectorPageStrengths.find('.marg1:nth-child(n+'+ (count + 1)+')').hide();
+            if(_sectorPageStrengths.find('.marg1').length > (count+1)) {
+                _sectorPageStrengths.find('.view-all-sectors-btn-container').show();
+            } else {
+                _sectorPageStrengths.find('.view-all-sectors-btn-container').hide();
+            }
+            _elements = count;
+        } else if(_vp == 1) {
+          _sectorPageStrengths.find('.marg1:nth-child(n+5)').hide();
+          if(_sectorPageStrengths.find('.marg1').length > 4) {
+              _sectorPageStrengths.find('.view-all-sectors-btn-container').show();
+          } else {
+              _sectorPageStrengths.find('.view-all-sectors-btn-container').hide();
+          }
+          _elements = 4;
+        } else {
+          _sectorPageStrengths.find('.marg1:nth-child(n+4)').hide();
+          _elements = 3;
+        }
+    }
 
     _bindShowMore = function(container){
         // if data-items, data-infinite is defined, used it
         var _showMore = $('.view-all-sectors-btn');
         _showMore.on('click',function(){
-              $('.sectorpage-strengths .container > .row + .row >.marg1:nth-child(2n+2)').toggle();
+              $('.sectorpage-strengths .container > .row + .row >.marg1:nth-child(n+'+ (_elements + 1) +')').toggle();
               $(this).parents('.sectorpage-strengths').toggleClass('showLess');
-            
+
         });
     }
 
     init = function() {
-              if (_sectorPageStrengths.length > 0) {
+        if (_sectorPageStrengths.length > 0) {
+            _checkElemnt();
             _bindShowMore(_sectorPageStrengths);
         }
     };
