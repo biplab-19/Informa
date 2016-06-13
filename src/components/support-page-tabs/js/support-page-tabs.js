4b310937ef85df8c61f2829b5124c07f0da0e80d
Tejaswi Chennupati <tchen3@sapient.com>
2016-06-13 14:48:08 +0530

    added new folder named support page tabs

---
+++ src/components/support-page-tabs/js/support-page-tabs.js
@@ -0,0 +1,31 @@
+/*
+* analyst-list.js
+*
+*
+* @project:    Informa
+* @date:       2016-May-22
+* @author:     Tejaswi tchennupati@sapient.com
+* @licensor:   SAPIENNITRO
+* @namespaces: INFORMA
+*
+*/
+
+var INFORMA = window.INFORMA || {};
+INFORMA.navbars = (function(window, $, namespace) {
+    'use strict';
+    //variables
+    var Tabs = $('.tabs-row ul.nav li'),
+        init;
+    init = function() {
+        jQuery(Tabs[0]).addClass('active');
+        Tabs.on('click', function() {
+            Tabs.removeClass('active');
+            jQuery(this).addClass('active');
+        })
+    };
+
+    return {
+        init: init
+    };
+}(this, jQuery, 'INFORMA'));
+jQuery(INFORMA.navbars.init());
