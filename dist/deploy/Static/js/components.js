c1c26677e7d15dc544d346d55fd69f855bde9fc8
Tejaswi Chennupati <tchen3@sapient.com>
2016-06-13 17:21:48 +0530

    dist

--- dist/deploy/Static/js/components.js
+++ dist/deploy/Static/js/components.js
@@ -703,113 +703,6 @@ INFORMA.navbars = (function(window, $, namespace) {
 jQuery(INFORMA.navbars.init());
 
 /*
- * Events Search.js
- *
- *
- * @project:    Informa
- * @date:       2016-May-5
- * @author:     Saurabh
- * @licensor:   SAPIENNITRO
- * @namespaces: INFORMA
- *
- */
-
-var INFORMA = window.INFORMA || {};
-INFORMA.EventsSearch = (function(window, $, namespace) {
-    'use strict';
-    //variables
-    var EventsLists = $('.events-search'),
-        Views = EventsLists.find('.views a'),
-        CalendarView = EventsLists.find('.views a.icon-calendar'),
-        Calendar = $('#events .container'),
-        MonthSelect = $('select[name="month"]'),
-        Urls = INFORMA.Configs.urls.webservices,
-        //methods
-        init, SwitchEvents, GetAjaxData, RenderMonthResults, SetCalendarEvents;
-
-    GetAjaxData = function (url, method, data, SCallback, Errcallback, SearchType) {
-        INFORMA.DataLoader.GetServiceData(url, {
-            method: method,
-            data: JSON.stringify({ data: data }),
-            success_callback: function (data) {
-                if (typeof SCallback === "function") {
-                    SCallback.call(this, data, SearchType);
-                }
-            },
-            error_callback: function () {
-                if (typeof Errcallback === "function") {
-                    Errcallback.call(this, data, SearchType);
-                }
-            }
-        });
-    },
-
-    SetCalendarEvents = function(eventList) {
-        Calendar.html("");
-        Calendar.fullCalendar({
-                header: {
-                    left: 'prev',
-                    center: 'title',
-                    right: 'next'
-                },
-                contentHeight: 1000,
-                eventRender: function(event, element, view) {
-                    var CurrentDate = new Date(),
-                        ItemDate = new Date(event.start._i);
-                    if(CurrentDate > ItemDate) {
-                        return $('<div class="events disabled"><p class="title"><a href="#">' + event.title + '</a></p><p class="country">' +event.location+ '</p></div>');
-                    } else if(CurrentDate == ItemDate) {
-                        return $('<div class="events current"><p class="title"><a href="#">' + event.title + '</a></p><p class="country">' +event.location+ '</p></div>');
-                    } else {
-                        return $('<div class="events"><p class="title"><a href="#">' + event.title + '</a></p><p class="country">' +event.location+ '</p></div>');
-                    }
-                },
-                events: eventList
-        });
-    },
-
-    RenderMonthResults = function(data){
-        var EventList = [];
-
-        for(var key in data) {
-            EventList.push({
-                "title": data[key].Title,
-                "start": data[key].EventStartDate,
-                "location": data[key].Location
-            })
-        }
-        // var Events = 
-        SetCalendarEvents(EventList);
-    },
-
-    SwitchEvents = function() {
-        Views.on('click', function() {
-            Views.removeClass('active');
-            jQuery(this).addClass('active');
-        })
-        CalendarView.on('click', function() {
-            Views.removeClass('active');
-            jQuery(this).addClass('active');
-            GetAjaxData(Urls.EventsSearch, "Get", null, RenderMonthResults, null, null);
-        })
-        MonthSelect.on('change', function() {
-            Calendar.fullCalendar('gotoDate', 2010, 6);
-        })
-    }
-
-    init = function() {
-        if(EventsLists.length > 0) {
-            SwitchEvents();
-        }
-    };
-
-    return {
-        init: init
-    };
-}(this, jQuery, 'INFORMA'));
-jQuery(INFORMA.EventsSearch.init());
-
-/*
  * feature-list.js
  *
  *
@@ -2573,11 +2466,11 @@ INFORMA.sectorPageStrengths = (function(window, $, namespace) {
 jQuery(INFORMA.sectorPageStrengths.init());
 
 /*
-* analyst-list.js
+* support-page-tabs.js
 *
 *
 * @project:    Informa
-* @date:       2016-May-22
+* @date:       2016-june-13
 * @author:     Tejaswi tchennupati@sapient.com
 * @licensor:   SAPIENNITRO
 * @namespaces: INFORMA
@@ -2585,16 +2478,21 @@ jQuery(INFORMA.sectorPageStrengths.init());
 */
 
 var INFORMA = window.INFORMA || {};
-INFORMA.navbars = (function(window, $, namespace) {
+INFORMA.navtabs = (function(window, $, namespace) {
     'use strict';
     //variables
-    var Tabs = $('.tabs-row ul.nav li'),
+    var Tabs = $('.support-page-tabs ul.nav li'),
+        tabcontent = $('.tab-content .tab-pane'),
         init;
     init = function() {
-        jQuery(Tabs[0]).addClass('active');
+        jQuery(Tabs[0]).addClass('active first-list');
+        jQuery(tabcontent[0]).addClass('active');
         Tabs.on('click', function() {
-            Tabs.removeClass('active');
+        Tabs.removeClass('active');
             jQuery(this).addClass('active');
+            var tabpane = jQuery(this).find('a').attr('href');
+            tabcontent.removeClass('active');
+            jQuery(tabpane).addClass('active');
         })
     };
 
@@ -2602,7 +2500,7 @@ INFORMA.navbars = (function(window, $, namespace) {
         init: init
     };
 }(this, jQuery, 'INFORMA'));
-jQuery(INFORMA.navbars.init());
+jQuery(INFORMA.navtabs.init());
 
 /*
  * training-material.js
--- src/components/support-page-tabs/js/support-page-tabs.js
+++ src/components/support-page-tabs/js/support-page-tabs.js
@@ -1,9 +1,9 @@
 /*
-* analyst-list.js
+* support-page-tabs.js
 *
 *
 * @project:    Informa
-* @date:       2016-May-22
+* @date:       2016-june-13
 * @author:     Tejaswi tchennupati@sapient.com
 * @licensor:   SAPIENNITRO
 * @namespaces: INFORMA
@@ -11,16 +11,21 @@
 */
 
 var INFORMA = window.INFORMA || {};
-INFORMA.navbars = (function(window, $, namespace) {
+INFORMA.navtabs = (function(window, $, namespace) {
     'use strict';
     //variables
-    var Tabs = $('.tabs-row ul.nav li'),
+    var Tabs = $('.support-page-tabs ul.nav li'),
+        tabcontent = $('.tab-content .tab-pane'),
         init;
     init = function() {
-        jQuery(Tabs[0]).addClass('active');
+        jQuery(Tabs[0]).addClass('active first-list');
+        jQuery(tabcontent[0]).addClass('active');
         Tabs.on('click', function() {
-            Tabs.removeClass('active');
+        Tabs.removeClass('active');
             jQuery(this).addClass('active');
+            var tabpane = jQuery(this).find('a').attr('href');
+            tabcontent.removeClass('active');
+            jQuery(tabpane).addClass('active');
         })
     };
 
@@ -28,4 +33,4 @@ INFORMA.navbars = (function(window, $, namespace) {
         init: init
     };
 }(this, jQuery, 'INFORMA'));
-jQuery(INFORMA.navbars.init());
+jQuery(INFORMA.navtabs.init());
