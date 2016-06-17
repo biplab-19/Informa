b023d18f19120f3ea206ceeaa3fe653c55a9c2be
Tejaswi Chennupati <tchen3@sapient.com>
2016-06-17 15:45:07 +0530

    ++hero banner for pdp

--- src/components/training-material/js/training-material.js
+++ src/components/training-material/js/training-material.js
@@ -46,30 +46,20 @@ INFORMA.trainingMaterial = (function(window, $, namespace) {
             speed: _speed,
             dots: _dots,
             adaptiveHeight: true,
-            arrows: false,
+            arrows: true,
             responsive: [{
                     breakpoint: 1024,
                     settings: {
-                        slidesToShow: 3,
-                        slidesToScroll: 3
+                        slidesToShow: 2,
+                        slidesToScroll: 2
                     }
-                }, {
-                    breakpoint: 600,
-                    settings: {
-                        slidesToShow: 3,
-                        slidesToScroll: 3
+                },{
+                        breakpoint: 480,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1
+                        }
                     }
-                }, {
-                    breakpoint: 480,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1,
-                        arrows: true
-                    }
-                }
-                // You can unslick at a given breakpoint now by adding:
-                // settings: "unslick"
-                // instead of a settings object
             ]
         });
     }
