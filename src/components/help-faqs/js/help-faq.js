b53c563df4798ed31dd2ace7bab403e61c330dd1
Santosh Ganthasetti <sganthasetti@sapient.com>
2016-06-20 12:51:56 +0530

    help faq's

---
+++ src/components/help-faqs/js/help-faq.js
@@ -0,0 +1,30 @@
+var INFORMA = window.INFORMA || {};
+INFORMA.helpfaq = (function(window, $, namespace) {
+    'use strict';
+    //variables
+    var _helpfaqSelect = $('.help-faq-select'),
+        _faqItemsWrapper = $('.help-faq-wrapper'),
+        // methods
+        init,
+        _showHideFaq;
+
+    _showHideFaq = function() {
+        _helpfaqSelect.change(function() {
+            var target = $(this).data('target');
+            $(target).children().removeClass('show').addClass('hide');
+            var show = $("option:selected", this).data('show');
+            $(show).removeClass('hide').addClass('show');
+        });
+    }
+
+    init = function() {
+        $('.help-faq-wrapper').children().first().addClass('show');
+        $('.help-faq-wrapper').children().not(':first').addClass('hide');
+        _showHideFaq();
+    };
+
+    return {
+        init: init
+    };
+}(this, jQuery, 'INFORMA'));
+jQuery(INFORMA.helpfaq.init());
