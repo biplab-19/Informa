f499326bd964e0f1cd8f0ee7eb2a5918da2d4a28
Saurabh Sinha <ssi253@sapient.com>
2016-06-20 22:33:41 +0530

    Included Resource Search HTMLS

---
+++ src/components/resource-page-filter/js/resource-filter.js
@@ -0,0 +1,64 @@
+/*
+ * Resource Filter.js
+ *
+ *
+ * @project:    Informa
+ * @date:       2016-April-25
+ * @author:     Rajiv
+ * @licensor:   SAPIENNITRO
+ * @namespaces: INFORMA
+ *
+ */
+
+var INFORMA = window.INFORMA || {};
+INFORMA.ResourceFilter = (function(window, $, namespace) {
+    'use strict';
+    //variables
+    var Templates = INFORMA.Templates,
+        ResourceContainer = $('.resource-filter'),
+        CustomSelect = ResourceContainer.find(".custom-multiselect select"),
+        SectorSelect = ResourceContainer.find("select.resource-sector"),
+        SubSectorSelect = ResourceContainer.find("select.resource-sub-sector"),
+    // methods
+        init,
+        BindDropDown;
+
+    BindDropDown = function() {
+            CustomSelect.val("");
+            CustomSelect.multiselect({
+                maxHeight: 200,
+                buttonText: function(o, s) {
+                    if (o.length === 0) {
+                        return $(s).data('placeholder');
+                    } else {
+                        var labels = 1;
+                        o.each(function(i) {
+                            labels = parseInt(1 + i);
+                        });
+                        return '<strong>'+labels + '</strong> Selected';
+                    }
+                },
+                onChange: function(option, checked, select) {
+                    if ($(option).parent().hasClass("resource-sector") === true) {
+                        if (checked) {
+                            SubSectorSelect.parents('li').removeClass('hidden');
+                        } else {
+                            SubSectorSelect.parents('li').addClass('hidden');
+                        }
+                    }
+                }
+            });
+        };
+    
+
+    init = function() {
+        if (ResourceContainer.length > 0) {
+            BindDropDown();
+        }
+    };
+
+    return {
+        init: init
+    };
+}(this, $INFORMA = jQuery.noConflict(), 'INFORMA'));
+jQuery(INFORMA.ResourceFilter.init());
