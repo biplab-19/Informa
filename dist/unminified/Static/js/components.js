0ec032bd1aea4353e79f286a7f910761a0382c5e
Santosh Ganthasetti <sganthasetti@sapient.com>
2016-06-21 15:16:29 +0530

    Build june-21

--- dist/unminified/Static/js/components.js
+++ dist/unminified/Static/js/components.js
@@ -3464,3 +3464,56 @@ INFORMA.trainingMaterial = (function(window, $, namespace) {
     };
 }(this, $INFORMA = jQuery.noConflict(), 'INFORMA'));
 jQuery(INFORMA.trainingMaterial.init());
+
+var INFORMA = window.INFORMA || {};
+INFORMA.videos = (function(window, $, namespace) {
+    'use strict';
+    //variables
+    var _videoFullWrapper = $('.video-img'),
+        _videoPlayBtnWrapper = $('.play-icon'),
+        video,
+        // methods
+        init,
+        _playFullVideoWrapper,
+        _playFullVideoBtnWrapper;
+
+    _playFullVideoBtnWrapper = function() {
+        _videoPlayBtnWrapper.click(function() {
+            var videoImg = $(this).parent().find('img');
+            if (videoImg.attr('data-videotype') == "youtube") {
+                video = '<iframe width="100%" src="' +  videoImg.attr('data-videourl') + '" frameborder="0" allowfullscreen></iframe>';
+            } else if (videoImg .attr('data-videotype') == "vimeo") {
+                video = '<iframe width="100%" src="' +  videoImg.attr('data-videourl') + '" frameborder="0" allowfullscreen></iframe>';
+            } else if (videoImg.attr('data-videotype') == "wistia") {
+                video = '<iframe width="100%" src="' +  videoImg.attr('data-videourl') + '" frameborder="0" allowfullscreen></iframe>';
+            }
+            videoImg.replaceWith(video);
+            $(this).remove();
+        });
+    }
+
+    _playFullVideoWrapper = function() {
+        _videoFullWrapper.click(function() {
+
+            if ($(this).attr('data-videotype') == "youtube") {
+                video = '<iframe width="100%" height="' + $(this).attr('height') + '" src="' + $(this).attr('data-videourl') + '" frameborder="0" allowfullscreen></iframe>';
+            } else if ($(this).attr('data-videotype') == "vimeo") {
+                video = '<iframe width="100%" height="' + $(this).attr('height') + '" src="' + $(this).attr('data-videourl') + '" frameborder="0" allowfullscreen></iframe>';
+            } else if ($(this).attr('data-videotype') == "wistia") {
+                video = '<iframe width="100%" height="' + $(this).attr('height') + '" src="' + $(this).attr('data-videourl') + '" frameborder="0" allowfullscreen></iframe>';
+            }
+            $(this).replaceWith(video);
+            $('.play-icon').remove();
+        });
+    }
+
+    init = function() {
+        _playFullVideoWrapper();
+        _playFullVideoBtnWrapper();
+    };
+
+    return {
+        init: init
+    };
+}(this, jQuery, 'INFORMA'));
+jQuery(INFORMA.videos.init());
