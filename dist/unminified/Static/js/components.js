a4ee7473905e81cedaa427087ac7108c0cbc5f61
Rajiv Aggarwal <raggarwal4@sapient.com>
2016-07-11 19:42:44 +0530

    updated

--- dist/unminified/Static/js/components.js
+++ dist/unminified/Static/js/components.js
@@ -4056,7 +4056,7 @@ INFORMA.SearchResults = (function(window, $, namespace) {
                     var results = {};
                     results.Results = data["Results"],
                     results.ProductFacets = data["ProductFacets"];
-                    RefineContainer.hide();
+                    RefineContainer.parent().hide();
                     ParseSearchData(results);
 
                     var UpdateTab = setTimeout(function() {
@@ -4270,13 +4270,12 @@ INFORMA.SearchResults = (function(window, $, namespace) {
                 }
                 if (Refine) { 
                    var html = CreateFilterList(Refine,Templates.ProductFacets,FilterLabels);
-                   //RefineContainer.show();
                    ShowFilter(html, RefineContainer ,false);
                    INFORMA.SearchResultFilter.DoRefine();
                 }else{
                     RefineContainer.html("");
-                    RefineContainer.hide();
                     $(".refine-list").off("click");
+                    RefineContainer.parent().parent().hide();
                 }
                 if(SearchTabs){
                     var Data = {} , html;
