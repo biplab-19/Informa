ab5f751d9778120ea5ca5a18e9531050cac52b72
Saurabh Sinha <ssi253@sapient.com>
2016-07-12 14:26:44 +0530

    Build CheckIn

--- dist/deploy/Static/js/components.js
+++ dist/deploy/Static/js/components.js
@@ -93,7 +93,8 @@ var INFORMA = window.INFORMA || {};
 INFORMA.analystProfile = (function(window, $, namespace) {
     'use strict';
      var init,
-        _bindShowMore;
+        _bindShowMore,
+        _checkButton;
 
     _bindShowMore = function(container){
         
@@ -104,9 +105,20 @@ INFORMA.analystProfile = (function(window, $, namespace) {
         });
     }
 
+    _checkButton = function () {
+        var ContentHeight = $('.descriptions').height(),
+            TotalHeight = $('.descriptions').addClass('show-content').height();
+            debugger;
+        if(TotalHeight <= ContentHeight) {
+            jQuery('.show-options').addClass('hidden');
+        }
+        $('.descriptions').removeClass('show-content');
+    }
+
     init = function() {
         //if (_analystList.length > 0) {
             _bindShowMore();
+            _checkButton();
         //}
     };
 
