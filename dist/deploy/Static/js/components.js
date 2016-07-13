91031dd9ec19fdef72df4ff7b5f6760d88a49e33
Santosh Ganthasetti <sganthasetti@sapient.com>
2016-07-13 19:03:35 +0530

    build ++

--- dist/deploy/Static/js/components.js
+++ dist/deploy/Static/js/components.js
@@ -3761,29 +3761,29 @@ INFORMA.RTETable = (function(window, $, namespace) {
     var titles = [],
         tablevalues = [],
         count, i,
-        rteTable = $('table.table'),
+        rteTable = $('.rte-custom-table table.table'),
         rteTableLength = rteTable.find("tr:first-child").children('td').length,
-        responsiveContainer = $(".responsiveTable"),
+        responsiveContainer = $(".rte-custom-table .responsiveTable"),
         init, _RTEOnLoad;
 
     _RTEOnLoad = function() {
         for (count = 2; count <= rteTableLength; count++) {
-            $(".table tr td:nth-child(" + count + ")").each(function() {
+            $(".rte-custom-table .table tr td:nth-child(" + count + ")").each(function() {
                 tablevalues.push("<tr><td>" + $(this).html() + "</td></tr>");
             });
             responsiveContainer.append('<table class="mobileTable">' + tablevalues.join(' ') + '</table>');
             tablevalues.length = 0;
         }
-        $(".table tr td:nth-child(1)").each(function() {
+        $(".rte-custom-table .table tr td:nth-child(1)").each(function() {
             titles.push("<td>" + $(this).html() + "</td>");
         });
-        $(".mobileTable").each(function() {
+        $(".rte-custom-table .mobileTable").each(function() {
             for (i = 0; i <= titles.length; i++) {
                 $(this).find('tr').eq(i).prepend(titles[i]);
             }
         });
-        $(".mobileTable tr:first-child td:first-child").remove();
-        $(".mobileTable tr:first-child td").attr('colspan', '2');
+        $(".rte-custom-table .mobileTable tr:first-child td:first-child").remove();
+        $(".rte-custom-table .mobileTable tr:first-child td").attr('colspan', '2');
     }
 
     init = function() {
