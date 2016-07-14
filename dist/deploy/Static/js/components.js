4997fed02b1dbfcf3f71dfd5293b6bd969006b13
Saurabh Sinha <ssi253@sapient.com>
2016-07-14 11:37:07 +0530

    Build CHeck In

--- dist/deploy/Static/js/components.js
+++ dist/deploy/Static/js/components.js
@@ -1384,7 +1384,7 @@ INFORMA.FAQs = (function (window, $, namespace) {
     },
 
     init = function () {
-        if (FaqMoreBtn.length > 0) {
+        if (AccordianWrapper.length > 0) {
             ResetAccordian();
             BindMore();
         }
