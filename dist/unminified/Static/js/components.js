31ffaa90020bc21a0fdb9aea471a47e1c6e2bcb3
Saurabh Sinha <ssi253@sapient.com>
2016-07-15 10:43:06 +0530

    Build latest

--- dist/unminified/Static/js/components.js
+++ dist/unminified/Static/js/components.js
@@ -1,4 +1,4 @@
-/*! 2016-07-14 */_adjustHeigt = function(){
+/*! 2016-07-15 */_adjustHeigt = function(){
   var maxHeightTitle = Math.max.apply(null, el.find('.sector-card h2').map(function() {
       return $(this).height();
   }).get());
@@ -1909,9 +1909,10 @@ INFORMA.formGetInTouch = (function(window, $, namespace) {
         });
     }
     _disableSubmit = function() {
-        $("form.get-in-touch .form-submit-border .btn, form.request-a-demo .form-submit-border .btn").attr('disabled', true);
+        $("form.get-in-touch .form-submit-border, form.request-a-demo .form-submit-border ").addClass('disabled');
+
         $("form.get-in-touch, form.request-a-demo").on('change', 'input, textarea, select, button, a', function() {
-            $('form.get-in-touch, form.request-a-demo').find('.form-submit-border .btn').removeAttr('disabled');
+            $('form.get-in-touch, form.request-a-demo').find('.form-submit-border').removeClass('disabled');
         });
     }
 
