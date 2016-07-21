dbfde56621340198df6ed6ce1c274c5f5f7560c1
Sarath Achyuta <sachyu@sapient.com>
2016-07-21 19:09:57 +0530

    build

--- dist/unminified/Static/js/components.js
+++ dist/unminified/Static/js/components.js
@@ -1704,7 +1704,7 @@ INFORMA.forms = (function(window, $, namespace) {
     }
 
     _resetForm = function($form) {
-        $form.find('input, select, textarea').val('');
+        $form.find('input:text, input:password, input:number, input:email, select, textarea').val('');
         $form.find('input:radio, input:checkbox')
              .removeAttr('checked').removeAttr('selected');
     }
@@ -2086,6 +2086,7 @@ INFORMA.forms = (function(window, $, namespace) {
         $(_formId).modal({ 
             show : 'true' 
         })
+        _showOverlay();
     };
 
     _bindProductId = function(){
@@ -3195,47 +3196,24 @@ var INFORMA = window.INFORMA || {};
 INFORMA.pdpBrochure = (function(window, $, namespace) {
     'use strict';
     //variables
-    var section = $('.pdp-brochure'),
-        anchorParent = section.find('brochure-btn'),
-        anchor = section.find('cta1'),
-        _formSubmitStatus = $('.form-status[data-successsubmit=True]'),
-    // methods
-        init,
-        _pdpBrochure,
-        _showResponse;
+   var downloadBrochure = $('.pdp-brochure'),
+       submitResponse = $('.submit-response'),
+       _formSubmitStatus = $('.form-status[data-successsubmit=True]'),
+    //methods
+        init;
 
-        _showResponse = function() {
-            if (_formSubmitStatus.length > 0) {
-                var formSubmitResponseModal = _formSubmitStatus.parents('.form-modal:first');
-                var url = $('.form-submit-border').attr("");
-                if (formSubmitResponseModal.length > 0) {
-                    formSubmitResponseModal.find('.page-header').addClass('hide');
-                    formSubmitResponseModal.modal({
-                        show: true,
-                        keyboard: false,
-                        backdrop: "static"
-                    })
-                    window.open("url", "_blank");
-                }
-                var formSubmitResponseHTML = _formSubmitStatus.parents('form:first');
-                if (formSubmitResponseHTML.length > 0) {
-                    formSubmitResponseHTML.find('.page-header').addClass('hide');
-                }
-            }
-        }
-//     $(document).ready(function(){
-//     $("button").click(function(){
-//         var x = $("form").serializeArray();
-//         $.each(x, function(i, field){
-//             $("#results").append(field.name + ":" + field.value + " ");
-//         });
-//     });
-// });
-init = function() {
-    _showResponse();
-};
+    init = function() {
+
+      if(submitResponse.length > 0){
+        window.open("https://www.agra-net.com/agra/foodnews/pdf-archive/market-reviews/article511004.ece/BINARY/FN-Juice-2016.pdf", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,left=100,width=1000,height=600");
+      }
+
+    }
+    return {
+        init: init
+    };
 }(this, jQuery, 'INFORMA'));
-jQuery(INFORMA.analystList.init());
+jQuery(INFORMA.pdpBrochure.init());
 
 /*
  * global-footer.js
