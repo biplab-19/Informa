40050359e86fd962621f3490b09ee79a8b2e904c
Santosh Ganthasetti <sganthasetti@sapient.com>
2016-07-26 13:17:55 +0530

    check areas of interest

--- src/components/form-wffm/js/form-wffm.js
+++ src/components/form-wffm/js/form-wffm.js
@@ -226,7 +226,7 @@ INFORMA.forms = (function(window, $, namespace) {
     }
 
     _parseResults = function(data) {
-        if (!$(_formId + ' fieldset').hasClass('.area-interests')) {
+        if (!$(_formId + ' fieldset').hasClass('area-interests')) {
             return false;
         }
         var results = data,
