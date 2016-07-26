50f575eb5befeecfd395936948faafbeb1e95f95
Santosh Ganthasetti <sganthasetti@sapient.com>
2016-07-26 22:51:02 +0530

    build

--- dist/unminified/Static/js/components.js
+++ dist/unminified/Static/js/components.js
@@ -3311,7 +3311,7 @@ var INFORMA = window.INFORMA || {};
 INFORMA.pdpBrochure = (function(window, $, namespace) {
     'use strict';
     //variables
-   var downloadBrochure = $('.pdp-brochure'),
+   var downloadBrochure = $('.pdp-brochure, #agri-report'),
        submitResponse = $('#formSendBrochure .submit-response'),
        Url = downloadBrochure.data('url'),
     //methods
