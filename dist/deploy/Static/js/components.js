d2fad9990fa13f84068ca8b73fafd3cce6e9aa5a
Tejaswi Chennupati <tchen3@sapient.com>
2016-08-11 12:11:25 +0530

    welcom description css and build

--- dist/deploy/Static/js/components.js
+++ dist/deploy/Static/js/components.js
@@ -3893,6 +3893,62 @@ jQuery(INFORMA.ProductFinder.init());
  *
  * @project:    Informa
  * @date:       2016-April-25
+ * @author:     Tejaswi
+ * @licensor:   SAPIENNITRO
+ * @namespaces: INFORMA
+ *
+ */
+
+var INFORMA = window.INFORMA || {};
+INFORMA.RecomendedTabs = (function(window, $, namespace) {
+    'use strict';
+    var RecomendedTab = $('.recommendation-tabs'),
+        RecomendedResults = $('.recommended-results'),
+        //methods
+        init, LargeDeviceFunction, SmallDeviceFunction;
+
+    SmallDeviceFunction = function (Parent) {
+        var Select = Parent.find('select[name="RecommendTabs"]'),
+            SelectFirst = $(Select.find('option')[0]);
+
+        Select.val('#Dashboard').trigger('change');
+
+        Select.on('change', function () {
+            var Value = $(this).val();
+            RecomendedResults.find('.tab-pane').removeClass('active');
+
+            $(Value).addClass('active');
+        })
+    }
+
+    LargeDeviceFunction = function (Parent) {
+        Parent.find('.nav-tabs li:first-child a').trigger('click');
+    },
+
+    init = function () {
+        if(RecomendedTab.length > 0) {
+            var Viewport = INFORMA.global.device.viewport;
+
+            if(Viewport == "mobile") {
+                SmallDeviceFunction(RecomendedTab);
+            } else {
+                LargeDeviceFunction(RecomendedTab);
+            }
+        }
+    }
+    return {
+        init: init
+    };
+
+}(this, $INFORMA = jQuery.noConflict(), 'INFORMA'));
+jQuery(INFORMA.RecomendedTabs.init());
+
+/*
+ * Product Results.js
+ *
+ *
+ * @project:    Informa
+ * @date:       2016-April-25
  * @author:     Rajiv Aggarwal
  * @licensor:   SAPIENNITRO
  * @namespaces: INFORMA
@@ -4005,6 +4061,7 @@ INFORMA.RecomendedContent = (function(window, $, namespace) {
 
     init = function () {
         if(RecomendedWrapper.length > 0) {
+            $('#Dashboard').addClass('active');
             equalHeight(RecomendedWrapper);
             ShowMore();
         }
