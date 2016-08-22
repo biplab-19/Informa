1ae9430ed527abd62d3b11715115a4205c92c19e
Tejaswi Chennupati <tchen3@sapient.com>
2016-08-22 17:30:52 +0530

    js and sass for recommended products

---
+++ src/components/recom-products/js/recom-products.js
@@ -0,0 +1,76 @@
+/*
+ * My recommendations-recommended products
+ *
+ *
+ * @project:    Informa
+ * @date:       2016-August-22
+ * @author:     Tejaswi chennupati
+ * @licensor:   SAPIENNITRO
+ * @namespaces: INFORMA
+ *
+ */
+
+var INFORMA = window.INFORMA || {};
+INFORMA.news_flash = (function(window, $, namespace) {
+    'use strict';
+    //variables
+    var _recommendedproducts = $('.recommended-products'),
+        _productsList = _recommendedproducts.find('.recom-prod-carousel'),
+    // methods
+        init,
+        _createSlider;
+        _createSlider = function(container){
+            // if data-items, data-infinite is defined, used it
+            var _slideCount = container.data('itemsperframe'),
+                _autoplay = container.data('autorotate'),
+                _speed = container.data('transitionspeed'), // speed of transition
+                _duration = container.data('slideduration'), // how long the slider will be displayed
+                _infinite = true,
+                _dots = Boolean(container.data('pagination'));
+                //chk for sitecore preview
+                if (INFORMA.global.siteCore.isPreview) {
+                    _autoplay = true;
+                }
+                if (INFORMA.global.siteCore.isExperience) {
+                    _autoplay = false;
+                    _infinite = false;
+                }
+
+            container.slick({
+                infinite: _infinite,
+                autoplay: _autoplay,
+                autoplaySpeed: _duration,
+                slidesToShow: _slideCount,
+                slidesToScroll: _slideCount,
+                speed: _speed,
+                dots: _dots,
+                adaptiveHeight: true,
+                arrows: false,
+                responsive: [{
+                        breakpoint: 1024,
+                        settings: {
+                            slidesToShow: 2,
+                            slidesToScroll: 2
+                        }
+                    },{
+                            breakpoint: 480,
+                            settings: {
+                                slidesToShow: 1,
+                                slidesToScroll: 1
+                            }
+                        }
+                ]
+            });
+        }
+
+    init = function() {
+        if (_productsList.length > 0) {
+            _createSlider(_productsList);
+        }
+    }
+
+    return {
+        init: init
+    }
+}(this, $INFORMA = jQuery.noConflict(), 'INFORMA'));
+jQuery(INFORMA.news_flash.init());
