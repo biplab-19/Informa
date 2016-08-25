f5a7d502108604b8dbef5d89f6395cdeca24ce17
Santosh Ganthasetti <sganthasetti@sapient.com>
2016-08-25 18:49:08 +0530

    form registration myinterests tabs

--- src/components/form-registration-myinterests/js/form-registration-myinterests.js
+++ src/components/form-registration-myinterests/js/form-registration-myinterests.js
@@ -2,51 +2,120 @@ var INFORMA = window.INFORMA || {};
 INFORMA.RegistrationInterests = (function(window, $, namespace) {
     'use strict';
     //variables
-    var init, _showProgressiveTabs, _renderMultiSelect, _showNextTab, _showPrevTab, _validateForm;
+    var init, _showProgressiveTabs,
+    _renderMultiSelect,
+    _showNextTab,
+    _showPrevTab,
+    _validateForm,
+    _appendNextBtn,
+    _appendBackBtn,
+    _myinterestForm = $('.register-myinterests-form'),
+    _myinterestFormContainer = $('.register-myinterests-form-container'),
+    _myinterestFormTabContainer = _myinterestFormContainer.find('tab-content'),
+    _stepOneContaner = _myinterestFormContainer.find('#step1'),
+    _stepTwoContaner = _myinterestFormContainer.find('#step2'),
+    _recommendedTips = $('.recommended-tips'),
+    _recommendedTipsContainer = $('.recommended-tips-container'),
+    _appendSteps,
+    _appendStepTwo,
+    _wrapFormContainer,
+    _renderAllContainers,
+    _renderRecommendedTips;
 
     //methods
+    _renderRecommendedTips = function(){
+      _recommendedTipsContainer.append(_recommendedTips).css('display', 'none');
+
+    }
+    _renderAllContainers = function(){
+        _myinterestForm.append(_myinterestFormContainer);
+        _myinterestForm.addClass('row');
+        _renderMultiSelect();
+    }
+    _wrapFormContainer = function(){
+      _myinterestFormContainer.before(_myinterestForm);
+    }
+    _appendSteps = function(){
+        var step1Block =_myinterestForm.find('fieldset.step1'), step2Block = step1Block.nextAll();
+        _stepOneContaner.prepend(step1Block);
+        _stepTwoContaner.prepend(step2Block);
+      //    aboutYouBlock.remove();
+    }
+    _appendStepTwo = function(){
+        var step2Block =_myinterestForm.find('.step2');
+        if(step2Block.length > 0 ){
+          if($.isArray(step2Block)){
+              $.each(step2Block, function(i){
+                  _stepTwoContaner.prepend($(this));
+              });
+          }else{
+            _stepTwoContaner.prepend(step2Block);
+          }
+        }
+
+        var submitBlock = _myinterestForm.find('.form-submit-border');
+       _stepTwoContaner.append(submitBlock);
+    }
+    _appendBackBtn = function(){
+        var backBtn = $('.prev-step')[0], btnContainer = _myinterestForm.find(":submit").parent();
+        btnContainer.append(backBtn);
+    }
+    _appendNextBtn = function(){
+        var nextBtn = '<ul class="list-inline pull-right"><li><button type="button" class="btn btn-primary next-step">Next</button></li></ul>';
+        $('.form-progressive-container').find('#step1').append(nextBtn);
+    }
+
     _showNextTab = function(elem) {
         $(elem).next().find('a[data-toggle="tab"]').click();
+        _recommendedTipsContainer.css('display', 'block');
     }
     _showPrevTab = function(elem) {
         $(elem).prev().find('a[data-toggle="tab"]').click();
+        _recommendedTipsContainer.css('display', 'none');
     }
 
     _renderMultiSelect = function() {
-        var findMultipleSelect = $('.register-myinterests').find('form select');
+        var findMultipleSelect = _myinterestForm.find('select');
         if (findMultipleSelect.length > 0) {
             $.each(findMultipleSelect, function(i) {
                 if ($(this).attr('multiple') == 'multiple') {
-                    $(this).multiselect();
+                    $(this).multiselect({
+                      includeSelectAllOption: true,
+                      maxHeight: 200
+                    });
                 }
             });
         }
     }
 
     _showProgressiveTabs = function() {
-        $('.triangle-nav > li a[title]').tooltip();
+      //  $('.triangle-nav > li a[title]').tooltip();
         $('a[data-toggle="tab"]').on('show.bs.tab', function(e) {
             var $target = $(e.target);
+            if($target.attr('href') == "#step2"){
+              _recommendedTipsContainer.css('display', 'block');
+            }else{
+              _recommendedTipsContainer.css('display', 'none');
+            }
             if ($target.parent().hasClass('disabled')) {
                 return false;
             }
         });
 
-        $(".next-step").click(function(e) {
+        $(".next-step").on('click', function(e) {
             var $active = $('.form-progressive-wizard .triangle-nav li.active');
             $active.next().removeClass('disabled');
             _validateForm();
             _showNextTab($active);
 
         });
-        $(".prev-step").click(function(e) {
+        $(".prev-step").on('click', function(e) {
             var $active = $('.form-progressive-wizard .triangle-nav li.active');
             _showPrevTab($active);
         });
     }
     _validateForm = function() {
-        var myinterestForm = $('.register-myinterests').find('form');
-        myinterestForm.validate({
+        _myinterestForm.validate({
             submitHandler: function() {
                 console.log("submitted!");
             },
@@ -61,7 +130,14 @@ INFORMA.RegistrationInterests = (function(window, $, namespace) {
 
     init = function() {
         _showProgressiveTabs();
+        // //_appendNextBtn();
+         _appendBackBtn();
+         _appendSteps();
+        // _appendStepTwo();
+         _wrapFormContainer();
+         _renderAllContainers();
         _renderMultiSelect();
+        _renderRecommendedTips();
         //  _validateForm();
     };
 
