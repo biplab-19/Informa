fcf96bdd035475b9a0585a11ed8c70b64b936717
Nupur Goyal <ngoya5@sapient.com>
2016-09-05 11:00:17 +0530

    bud fixes

--- dist/deploy/Static/js/main.js
+++ dist/deploy/Static/js/main.js
@@ -1,4 +1,4 @@
-/*! 2016-09-04 */s = new AppMeasurement()
+/*! 2016-09-05 */s = new AppMeasurement()
 //s.account="informashopwindowpharmadev" // QA
 s.account="informashopwindowpharmapreprod" // UAT
 //s.account="informashopwindowpharmaprod" // Prod
