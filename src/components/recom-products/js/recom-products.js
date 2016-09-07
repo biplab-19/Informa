2e5c9f13b49b8a911f75980f8577fa000131953c
Saurabh Sinha <ssi253@sapient.com>
2016-09-07 11:30:41 +0530

    Build Check In

--- src/components/recom-products/js/recom-products.js
+++ src/components/recom-products/js/recom-products.js
@@ -22,8 +22,9 @@ INFORMA.news_flash = (function(window, $, namespace) {
         EqualHeightProducts;
 
         EqualHeightProducts = function() {
-            var Items = _recommendedproducts.find('.wrap-content');
-            
+            var Items = _recommendedproducts.find('.wrap-content'),
+                MaxHeight = 0;
+                Items.height('auto');
                 Items.each(function() {
                     var ItemHeight = $(this).outerHeight();
                     if (ItemHeight > MaxHeight) {
@@ -80,7 +81,9 @@ INFORMA.news_flash = (function(window, $, namespace) {
     init = function() {
         if (_productsList.length > 0) {
             _createSlider(_productsList);
-            EqualHeightProducts();
+            setTimeout(function() {
+                EqualHeightProducts();
+            }, 500);
         }
     }
 
