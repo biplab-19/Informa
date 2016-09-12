3aa84613da559553adfdcd4acf35a3c35d74f2f9
Saurabh Sinha <ssi253@sapient.com>
2016-09-13 01:19:46 +0530

    Build++

--- dist/unminified/Static/js/main.js
+++ dist/unminified/Static/js/main.js
@@ -1,4 +1,4 @@
-/*! 2016-09-12 */var INFORMA = window.INFORMA || {};
+/*! 2016-09-13 */var INFORMA = window.INFORMA || {};
 (function(window, $, namespace) {
     'use strict';
     var env = (window.location.href.indexOf("127.0.0.1") > -1) ? "local" : "dev",
