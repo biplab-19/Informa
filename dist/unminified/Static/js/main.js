74a095badd57155920233ed92d8d1d9c375e4d90
Rajiv Aggarwal <raggarwal4@sapient.com>
2016-09-18 19:03:47 +0530

    Variable cleaned and updated

--- dist/unminified/Static/js/main.js
+++ dist/unminified/Static/js/main.js
@@ -1,4 +1,4 @@
-/*! 2016-09-17 */var INFORMA = window.INFORMA || {};
+/*! 2016-09-18 */var INFORMA = window.INFORMA || {};
 (function(window, $, namespace) {
     'use strict';
     var env = (window.location.href.indexOf("127.0.0.1") > -1) ? "local" : "dev",
