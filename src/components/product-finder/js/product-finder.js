7e867a2e78a6126527040ed3b521ea2ad8e034f0
Rajiv Aggarwal <raggarwal4@sapient.com>
2016-09-25 15:35:38 +0530

    SW-7336

--- src/components/product-finder/js/product-finder.js
+++ src/components/product-finder/js/product-finder.js
@@ -55,6 +55,11 @@ INFORMA.ProductFinder = (function(window, $, namespace) {
                             scrollTop: 0
                         }, 600);
                     }
+                    if($(".mobileNavigation").hasClass("navbar-fixed-top")===true){
+                        $('html, body').stop().animate({
+                            scrollTop: 0
+                        }, 600);
+                    }
                 }
                 
             });
