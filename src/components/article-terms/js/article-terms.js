f57ff4600a00c53e4515ffbd70a9789631c48616
Nupur Goyal <ngoya5@sapient.com>
2016-10-17 12:48:29 +0530

    SW-8027

--- src/components/article-terms/js/article-terms.js
+++ src/components/article-terms/js/article-terms.js
@@ -5,17 +5,31 @@ INFORMA.freearticle = (function(window, $, namespace) {
     var init,
         _setFreearticlePadding;
     _setFreearticlePadding = function() {
-        var freearticleSection = $('#productDetails section.articlepage-freearticle');
+        var freearticleSection = $('#productDetails').find('.articlepage-freearticle'),
+            freearticleH2 = freearticleSection.find('h2'),
+            freearticleH3 = freearticleSection.find('h3');
         if (freearticleSection.length > 0) {
             if (INFORMA.global.device.viewportN == 1) {
-                freearticleSection.first().css('padding-top', '40px');
-                freearticleSection.last().css('padding-bottom', '40px');
+                freearticleSection.first().css('padding-top', '35px');
+                freearticleSection.last().css('padding-bottom', '35px');
+                freearticleH2.first().css('padding-top', '0');
+                freearticleH2.last().css('padding-bottom', '35px');
+                freearticleH3.first().css('padding-top', '0');
+                freearticleH3.last().css('padding-bottom', '25px');
             } else if (INFORMA.global.device.viewportN == 2) {
-                freearticleSection.first().css('padding-top', '30px');
-                freearticleSection.last().css('padding-bottom', '30px');
+                freearticleSection.first().css('padding-top', '25px');
+                freearticleSection.last().css('padding-bottom', '25px');
+                freearticleH2.first().css('padding-top', '0');
+                freearticleH2.last().css('padding-bottom', '25px');
+                freearticleH3.first().css('padding-top', '0');
+                freearticleH3.last().css('padding-bottom', '20px');
             } else {
-                freearticleSection.first().css('padding-top', '50px');
-                freearticleSection.last().css('padding-bottom', '50px');
+                freearticleSection.first().css('padding-top', '45px');
+                freearticleSection.last().css('padding-bottom', '45px');
+                freearticleH2.first().css('padding-top', '0');
+                freearticleH2.last().css('padding-bottom', '45px');
+                freearticleH3.first().css('padding-top', '0');
+                freearticleH3.last().css('padding-bottom', '30px');
             }
         }
     }
