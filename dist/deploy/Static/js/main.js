e98f91fb192f974f18906349bc894cd8b7bda6d2
Nupur Goyal <ngoya5@sapient.com>
2016-10-26 12:05:31 +0530

    build++

--- dist/deploy/Static/js/main.js
+++ dist/deploy/Static/js/main.js
@@ -1,4 +1,4 @@
-/*! 2016-10-25 */s = new AppMeasurement()
+/*! 2016-10-26 */s = new AppMeasurement()
 //s.account="informashopwindowpharmadev" // QA
 s.account="informashopwindowpharmapreprod" // UAT
 //s.account="informashopwindowpharmaprod" // Prod
--- dist/unminified/Static/js/main.js
+++ dist/unminified/Static/js/main.js
@@ -1,4 +1,4 @@
-/*! 2016-10-25 */var INFORMA = window.INFORMA || {};
+/*! 2016-10-26 */var INFORMA = window.INFORMA || {};
 (function(window, $, namespace) {
     'use strict';
     var env = (window.location.href.indexOf("127.0.0.1") > -1) ? "local" : "dev",
