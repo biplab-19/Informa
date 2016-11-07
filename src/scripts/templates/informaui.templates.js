198238c0fbf7c18d89ce096de8aaffca9129b2cb
SAPIENT\ragga3 <raggarwal4@sapient.com>
2016-11-07 13:55:54 +0530

    Mutiple Product chnages

--- src/scripts/templates/informaui.templates.js
+++ src/scripts/templates/informaui.templates.js
@@ -109,7 +109,7 @@ var INFORMA = window.INFORMA || {};
         'HeadlinesListItems':
             '{{#each Headlines}}'+
                 '<li>'+
-                    '{{#compare Product null operator="!="}}'+
+                    '{{#compare Products null operator="!="}}'+
                         '<p class="type">'+
                             '{{#each Products}}'+
                                 '<span>{{this}}</span>'+
@@ -492,9 +492,9 @@ var INFORMA = window.INFORMA || {};
                                                 '{{/each}}'+
                                             '</p>'+
                                         '{{/compare}}'+
-                                        '{{#compare Products.length "0" operator=">"}}'+
+                                        '{{#compare results.Products null operator="!="}}'+
                                             '<p class="type">'+
-                                                '{{#each Products}}'+
+                                                '{{#each results.Products}}'+
                                                     '<span>{{this}}</span>'+
                                                 '{{/each}}'+
                                             '</p>'+
