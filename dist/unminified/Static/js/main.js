1f7e1f081fa18c4211e2d39b1cb7ef0c4748388d
Santosh Ganthasetti <sganthasetti@sapient.com>
2016-11-16 14:24:35 +0530

    Build

--- dist/unminified/Static/js/main.js
+++ dist/unminified/Static/js/main.js
@@ -1,4 +1,4 @@
-/*! 2016-11-15 */var INFORMA = window.INFORMA || {};
+/*! 2016-11-16 */var INFORMA = window.INFORMA || {};
 (function(window, $, namespace) {
     'use strict';
     var env = (window.location.href.indexOf("127.0.0.1") > -1) ? "local" : "dev",
