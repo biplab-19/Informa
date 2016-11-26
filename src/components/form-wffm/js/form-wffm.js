a43a7d26e8e85076cf1506a66fa06d60289a11d7
Saurabh Sinha <ssi253@sapient.com>
2016-11-26 19:41:46 +0530

    Form change patch

--- src/components/form-wffm/js/form-wffm.js
+++ src/components/form-wffm/js/form-wffm.js
@@ -689,7 +689,7 @@ INFORMA.forms = (function(window, $, namespace) {
         ProductDropdown.append('<option val="' +name+ '">' +name+ '</option>');
         ProductDropdown.val(name);
         ProductDropdown.trigger('chosen:updated');
-        ProductDropdown.parents('.form-group:first').addClass('disable-dropdown');
+        ProductDropdown.parents('.form-group').addClass('disable-dropdown');
         
     };
 
