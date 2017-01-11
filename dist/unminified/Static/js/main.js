41ff24a602567d4c201be90db89e76076bce8362
Nupur Goyal <ngoya5@sapient.com>
2017-01-11 18:30:16 +0530

    ISW-249

--- dist/unminified/Static/js/main.js
+++ dist/unminified/Static/js/main.js
@@ -8844,7 +8844,8 @@ INFORMA.globalHeader = (function(window, $, namespace) {
         _cookieBannerExist,
         _PdpNavReArrange,
         _addClassFixed,
-        _removeClassFixed;
+        _removeClassFixed,
+        _pdpListItemScroll;
 
 
 
@@ -8909,6 +8910,23 @@ INFORMA.globalHeader = (function(window, $, namespace) {
         _cookieBanner.removeClass(_fixed);
         _mobileNavigation.css('top', 0);
         $('body').css('padding-top', 0);
+   }
+
+   //scroll pdp list item
+   _pdpListItemScroll = function(){
+        var pdpListHeight = $('#pdp-sections ul li').height()*$('#pdp-sections ul li').length;
+        var pdpSectionheight = $(window).height() - _navHeightMobile - $('#pdp-navigation .nav-pdp-nondesktop').outerHeight() - _cookieHeight;
+        var pdpHeadingHeight = $('#pdp-sections-heading').height();
+        if((pdpListHeight + pdpHeadingHeight) > pdpSectionheight){
+            $('#pdp-sections').height(pdpSectionheight);
+            $('#pdp-sections').css('overflow' , 'auto');
+        } 
+        else{
+            $('#pdp-sections').css({
+                'height':'auto',
+                'overflow':'hidden'
+            })
+        }
    }
 
     // both pdp nav and main nav handled here
@@ -8980,19 +8998,7 @@ INFORMA.globalHeader = (function(window, $, namespace) {
                 $('#pdp-sections').slideDown(function() {
                     if(!INFORMA.global.device.isDesktop){
                         if(_pdpNavigation.hasClass(_fixed)){
-                            var pdpHeight = $('#pdp-sections ul li').height()*$('#pdp-sections ul li').length;
-                            var height = $(window).height() - _navHeightMobile - $('#pdp-navigation .nav-pdp-nondesktop').outerHeight() - _cookieHeight;
-                            var pdpHeadingHeight = $('#pdp-sections-heading').height();
-                            if((pdpHeight + pdpHeadingHeight) > height){
-                                $('#pdp-sections').height(height);
-                                $('#pdp-sections').css('overflow' , 'auto');
-                            } 
-                            else{
-                                $('#pdp-sections').css({
-                                    'height':'auto',
-                                    'overflow':'hidden'
-                                })
-                            }
+                            _pdpListItemScroll();
                             $('#pdp-sections').animate({
                                     scrollTop: 0
                             }, 500);
@@ -9112,13 +9118,7 @@ INFORMA.globalHeader = (function(window, $, namespace) {
                 }
                 $('.nav-pdp-nondesktop-sticky').addClass('move-left');
                 _pdpMenuFollower.css('left', leftOfPdpMover + $('#pdp-sections-heading').outerWidth());
-                var pdpHeight = $('#pdp-sections ul li').height()*$('#pdp-sections ul li').length;
-                var height = $(window).height() - _navHeightMobile - $('#pdp-navigation .nav-pdp-nondesktop').outerHeight() - _cookieHeight;
-                if(pdpHeight > height){
-                    $('#pdp-sections').height(height);
-                    $('#pdp-sections').css('overflow' , 'auto');
-                } 
-                   
+                _pdpListItemScroll();
             }
 
             if (_arrayFlag) {
