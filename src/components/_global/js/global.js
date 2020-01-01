970e8f2c6cbcb965114d1a1bb68a667891fa8edc
Mohit Gupta <Mohit.Gupta@informa.com>
2020-01-01 21:07:25 +0000

    Fix formating

--- src/components/_global/js/global.js
+++ src/components/_global/js/global.js
@@ -11,43 +11,43 @@
  */
 
 var INFORMA = window.INFORMA || {};
-INFORMA.global = (function(window, $, namespace) {
-	'use strict';
-	//variables
-	var device = {},
-		siteCore = {},
-		_html = $('html');
+INFORMA.global = (function (window, $, namespace) {
+    'use strict';
+    //variables
+    var device = {},
+        siteCore = {},
+        _html = $('html');
 
 
-	var init = function(){
-		// viewport properties
-		var _viewportWidth = $(window).width();
-		if(_viewportWidth > 1024){
-			device.isDesktop = true;
-			device.viewport = 'desktop';
-			device.viewportN = 0;
-		}
-		else if(_viewportWidth >= 768){
-			device.isTablet = true;
-			device.viewport = 'tablet';
-			device.viewportN = 1;
-		}
-		else {
-			device.isMobile = true;
-			device.viewport = 'mobile';
-			device.viewportN = 2;
-		}
-		_html.addClass(device.viewport);
+    var init = function () {
+        // viewport properties
+        var _viewportWidth = $(window).width();
+        if (_viewportWidth > 1024) {
+            device.isDesktop = true;
+            device.viewport = 'desktop';
+            device.viewportN = 0;
+        }
+        else if (_viewportWidth >= 768) {
+            device.isTablet = true;
+            device.viewport = 'tablet';
+            device.viewportN = 1;
+        }
+        else {
+            device.isMobile = true;
+            device.viewport = 'mobile';
+            device.viewportN = 2;
+        }
+        _html.addClass(device.viewport);
 
-		// siteCore properties
-		if( $('html').hasClass('preview-mode')){
-			siteCore.isPreview = true;
-		}
-		else if($('html').hasClass('experience-mode')){
-			siteCore.isExperience = true;
-		}
-		
-		//Creating session cookies to maintain referral 
+        // siteCore properties
+        if ($('html').hasClass('preview-mode')) {
+            siteCore.isPreview = true;
+        }
+        else if ($('html').hasClass('experience-mode')) {
+            siteCore.isExperience = true;
+        }
+
+        //Creating session cookies to maintain referral 
         $(document).ready(function () {
             var docCookies = document.cookie;
             var docCookiesArray = docCookies.split(';');
@@ -64,12 +64,12 @@ INFORMA.global = (function(window, $, namespace) {
                 document.cookie = 'document-referrer=' + docReferrerValue;
             }
         });
-	}
+    }
 
-	return {
-		init: init,
-		device: device,
-		siteCore: siteCore
-	};
+    return {
+        init: init,
+        device: device,
+        siteCore: siteCore
+    };
 }(this, $INFORMA = jQuery.noConflict(), 'INFORMA'));
 jQuery(INFORMA.global.init());
