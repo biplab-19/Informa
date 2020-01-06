d9fd16ab072e25657203ef1d4690f6571ce71b0b
Rahul Gaur <rahul.gaur@yendigital.com>
2020-01-06 20:13:27 +0530

    fixes for newco design

--- src/components/articles-headlines/js/article-headlines.js
+++ src/components/articles-headlines/js/article-headlines.js
@@ -4,30 +4,34 @@ var INFORMA = window.INFORMA || {};
 INFORMA.articleHeadline = (function (window, $, namespace) {
     'use strict';
     //variables
-    var init;
+    var  categoryChild=$(".all-category").children("a"),
+         init;
     init = function () {
         $(document).ready(function () {
             //checking the gradient required or not
             if ($('.drop-content #all-category')[0].checked) {
-                $(".all-category").children("a").removeClass("select-gradient");
+                categoryChild.removeClass("select-gradient");
             } else {
-                $(".all-category").children("a").addClass("select-gradient");
+                categoryChild.addClass("select-gradient");
             }
             //on click of all category
             $(document).on("click", ".all-category a", function () {
+                var thisChild =$(this).children("a").children("span"),
+                    thisParent=$(this).parent().hasClass('active'),
+                    thisSpan=$(this).children("span");
                 $(this).removeClass("active");
                 $(this).children(".dropdown-content").removeClass("drop-content-active");
-                $(this).children("a").children("span").removeClass('triangle-down');
-                $(this).children("a").children("span").addClass('triangle-right');
+                thisChild.removeClass('triangle-down');
+                thisChild.addClass('triangle-right');
                 $(this).parent().toggleClass('active');
                 $(this).siblings(".dropdown-content").toggleClass('drop-content-active');
-                if ($(this).parent().hasClass('active')) {
-                    $(this).children("span").removeClass('triangle-right');
-                    $(this).children("span").addClass('triangle-down');
+                if (thisParent) {
+                    thisSpan.removeClass('triangle-right');
+                    thisSpan.addClass('triangle-down');
                 }
                 else {
-                    $(this).children("span").addClass('triangle-right');
-                    $(this).children("span").removeClass('triangle-down');
+                    thisSpan.addClass('triangle-right');
+                    thisSpan.removeClass('triangle-down');
                 }
 
             });
@@ -37,9 +41,9 @@ INFORMA.articleHeadline = (function (window, $, namespace) {
                 var valueOfli = $('input[name="yesno"]:checked').parent(".custom-radio").siblings(".sub-seg").text();
                 $('a#category-value').html('<span class="triangle-down"></span>' + valueOfli);
                 if ($('.drop-content #all-category')[0].checked) {
-                    $(".all-category").children("a").removeClass("select-gradient");
+                    categoryChild.removeClass("select-gradient");
                 } else {
-                    $(".all-category").children("a").addClass("select-gradient");
+                    categoryChild.addClass("select-gradient");
                 }
             });
         });
