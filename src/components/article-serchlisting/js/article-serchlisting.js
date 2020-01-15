bd479976937751f1ea2de5da2c245d676be2e340
Girish Sharma <girish.sharma@yendigital.com>
2020-01-15 11:44:49 +0530

    Design fixes

--- src/components/article-serchlisting/js/article-serchlisting.js
+++ src/components/article-serchlisting/js/article-serchlisting.js
@@ -9,25 +9,30 @@ INFORMA.articleSerchlisting = (function(window, $, namespace) {
             $(document).on("click", ".btn-group .dropdown-toggle", function () {
                 $(this).parent(".btn-group").toggleClass("open");
                 if($(this).siblings(".multiselect-container").children("li").hasClass("active")){
-                    $(this).addClass("select-gradient");
+                    $(this).toggleClass("select-gradient");
                 }
                 else{
                     $(this).removeClass("select-gradient");
                 }
-                if($(this).hasClass("select-gradient")){
-                    $(this).parent(".btn-group").css("border" , "2px solid transparent");
-                }
-                else{
-                    $(this).parent(".btn-group").css("border" , "2px solid #a8abb5");
-                }
             });
            
             $(document).on("click", ".panel-default .panel-title a", function () {
-                $(this).toggleClass("toggle-panel-body");
+                 $(this).toggleClass("toggle-panel-body");
                 if($(this).parents(".panel-heading").siblings(".panel-collapse").hasClass("panel-collapse")){
                     $(this).parents(".panel-heading").siblings(".panel-collapse").toggleClass("open-collapse");
-                }
+                 }
             });
+            $(document).on("click", ".resource-sector-search .button button", function () {
+                $(this).parents(".resource-finder").siblings(".product-finder-results").addClass("show-refine-container");
+                $(this).parents(".resource-finder").siblings(".product-finder-results").find("#refine-container-sction").show();
+            });
+            $(document).on("click", ".close-refine-list", function (){
+                $(this).parent("#refine-container-sction").hide();
+              });
+            if($(".product-finder-results").children(".container").children(".refine-container").find(".heading").hasClass("heading-collapsed")){
+                $(".product-finder-results").children(".container").children(".refine-container").find(".heading").removeClass("heading-collapsed");
+                $(".product-finder-results").children(".container").children(".refine-container").find(".panel-group").css("display" , "block");
+            }
             $(".panel-default .panel-title a").parents(".panel-heading").siblings(".panel-collapse").removeClass("collapse");
         });
     };
