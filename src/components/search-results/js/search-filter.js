92ff35b9135ed1e555a42f766f342747f2123fbf
Rahul Gaur <rahul.gaur@yendigital.com>
2020-02-09 23:03:14 +0530

    SW-9292

--- src/components/search-results/js/search-filter.js
+++ src/components/search-results/js/search-filter.js
@@ -96,6 +96,14 @@ INFORMA.SearchResultFilter = (function (window, $, namespace) {
             if (SearchType === "ProductSearch") {
                 Data.IsProduct = true;
             }
+            Data.SearchText = getUrlParameter('searchtext');
+            var isnewco = $("#productListingPageUrl").val();
+            if (isnewco != undefined && isnewco != "") {
+                if (isnewco.indexOf("newco") != -1) {
+                    SearchType = "SearchResult";
+                }
+
+            }
             if (Data.SearchText) {
                 searchText = Data.SearchText;
             }
@@ -144,7 +152,20 @@ INFORMA.SearchResultFilter = (function (window, $, namespace) {
             }
 
         },
+        getUrlParameter = function getUrlParameter(sParam) {
+            var sPageURL = window.location.search.substring(1),
+                sURLVariables = sPageURL.split('&'),
+                sParameterName,
+                i;
 
+            for (i = 0; i < sURLVariables.length; i++) {
+                sParameterName = sURLVariables[i].split('=');
+
+                if (sParameterName[0] === sParam) {
+                    return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+                }
+            }
+        },
         getProductSearchParams = function () {
             var parameter, urlQueryStrings = [];
             if ($('#SectorNames').val()) {
