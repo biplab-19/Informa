8ba09d5169788df01f848a814cb8b80d63916063
Shahid <Jawaad.Shahid.GB@informa.com>
2020-02-19 17:31:21 +0000

    moved variables to be set after newco specific check

--- src/components/newco-header/js/newco-header.js
+++ src/components/newco-header/js/newco-header.js
@@ -14,24 +14,24 @@ var INFORMA = window.INFORMA || {};
 INFORMA.NewcoHeader = (function (window, $, namespace) {
     'use strict';
     var $body = $('body'),
-        $cookieBanner = $('#cookieBanner'),
-        $mainHeader = $('#informa-main-header'),
-        $newcoHeader = $mainHeader.find('.newco-header'),
-        $hamburger = $newcoHeader.find('.hamburger'),
-        $searchicon = $newcoHeader.find('button#mobile-search'),
-        $navsearch = $newcoHeader.find('.newco-search-header'),
-        $newcoNav = $newcoHeader.find('.newco-nav'),
-        $menuItems = $newcoNav.find('.menu-items'),
-        $menuItemWWithSubs = $newcoNav.find('.menu-item.hassub'),
-        $banner = $('#banner'),
-        $pdpNav = $('#pdp-navigation'),
-        headerHeight = $mainHeader.height(),
-        DESIRED_HEADER_HEIGHT = 80,
-        bannerHeight = $banner.height(),
-        pdpNavThreshold = headerHeight + bannerHeight,
-        pdpNavTop = 0,
-        pdpNavHeight = 0,
-        headerHeightChangeTimerEvent = 0,
+        $cookieBanner,
+        $mainHeader,
+        $newcoHeader,
+        $hamburger,
+        $searchicon,
+        $navsearch,
+        $newcoNav,
+        $menuItems,
+        $menuItemWWithSubs,
+        $banner,
+        $pdpNav,
+        headerHeight,
+        DESIRED_HEADER_HEIGHT,
+        bannerHeight,
+        pdpNavThreshold,
+        pdpNavTop,
+        pdpNavHeight,
+        headerHeightChangeTimerEvent,
         // methods
         OffsetParentHeight, repositionPdpNav, init;
 
@@ -66,6 +66,27 @@ INFORMA.NewcoHeader = (function (window, $, namespace) {
     init = function() {
         // don't run if we're not in newco
         if (!$body.hasClass('tmt-newco')) return;
+
+        // set variable values
+        $cookieBanner = $('#cookieBanner'),
+        $mainHeader = $('#informa-main-header'),
+        $newcoHeader = $mainHeader.find('.newco-header'),
+        $hamburger = $newcoHeader.find('.hamburger'),
+        $searchicon = $newcoHeader.find('button#mobile-search'),
+        $navsearch = $newcoHeader.find('.newco-search-header'),
+        $newcoNav = $newcoHeader.find('.newco-nav'),
+        $menuItems = $newcoNav.find('.menu-items'),
+        $menuItemWWithSubs = $newcoNav.find('.menu-item.hassub'),
+        $banner = $('#banner'),
+        $pdpNav = $('#pdp-navigation'),
+        headerHeight = $mainHeader.height(),
+        DESIRED_HEADER_HEIGHT = 80,
+        bannerHeight = $banner.height(),
+        pdpNavThreshold = headerHeight + bannerHeight,
+        pdpNavTop = 0,
+        pdpNavHeight = 0,
+        headerHeightChangeTimerEvent = 0,
+
         // set listner for burger button
         $hamburger.click(function() {
             $newcoHeader.toggleClass('nav-closed');
