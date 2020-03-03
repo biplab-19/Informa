cdd6cad9b6e38946d820eb1ade2b9258ce64cd67
Shahid <Jawaad.Shahid.GB@informa.com>
2020-03-03 11:06:14 +0000

    equal height applied to analyst tiles

--- src/components/analyst-tile-visual-design/js/analyst-tile-visual-design.js
+++ src/components/analyst-tile-visual-design/js/analyst-tile-visual-design.js
@@ -1,1 +1,29 @@
+var INFORMA = window.INFORMA || {};
+INFORMA.NewcoAnalystTiles = (function(window, $, namespace) {
+    'use strict';
+    //variables
+    var $analystSection = $('.newco-analyst-views'),
+        $tileContainer = $analystSection.find('.analyst-view-container'),
+        //methods
+        init, equalHeight;
 
+    equalHeight = function() {
+        var maxheight = 0,
+            $profileDescr;
+        $tileContainer.each(function() {
+            $profileDescr = $(this).find('.profile-discription');
+            if ($profileDescr.height() > maxheight)
+                maxheight = $profileDescr.height();
+        });
+        $tileContainer.find('.profile-discription').css('height', maxheight);
+    }
+
+    init = function() {
+        equalHeight();
+    };
+
+    return {
+        init: init
+    };
+}(this, jQuery, 'INFORMA'));
+jQuery(INFORMA.NewcoAnalystTiles.init());
