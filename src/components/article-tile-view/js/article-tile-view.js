d0f49726aabc17900576b26c6bca11d05cef8ac8
Biplab Ram <jetsavvy.design01@outlook.com>
2020-07-22 14:32:53 +0530

    SW-9484

--- src/components/article-tile-view/js/article-tile-view.js
+++ src/components/article-tile-view/js/article-tile-view.js
@@ -1,45 +1,58 @@
 var INFORMA = window.INFORMA || {};
 INFORMA.articleTileView = (function (window, $, namespace) {
-    'use strict';
+  "use strict";
 
-    $('.carousel-container').slick({
-        vertical: true,
-        verticalSwiping: true,
-    });
+  $(".carousel-container").slick({
+    vertical: true,
+    verticalSwiping: true,
+    infinite: false,
+    loop: false,
+  });
 
-    $(".pc-body.Podcastslist #podcast-recoderbutton").click(function () {
-        $(".podcast-recoder").hide();
-        $("._brexitstyle .carousel-section").show();
-    });
+  $(".pc-body.Podcastslist .podcast-recoderbutton").click(function () {
+   $(this).parent(".podcast-recoder").hide();
+   $(this).parent(".podcast-recoder").siblings(".carousel-section").show();
 
-    $("#podcastsidbutton").click(function () {
-        $("._brexitstyle #Podcastslistboxone").addClass("demo");
-    });
+  });
 
-    $("#podcastsidbuttontwo").click(function () {
-        $("._brexitstyle #Podcastslistboxtwo").addClass("demo");
-    });
+  $(".btn-Podcasts").click(function () {
+    $(this).closest(".pc-body").next("div").addClass("demo");
+  });
 
-    $("#podcastsidbuttonthree").click(function () {
-        $("._brexitstyle #Podcastslistboxthree").addClass("demo");
-    });
+  $("button#carouselbutton").click(function () {
+    $(this).closest(".demo").removeClass("demo");
+  });
 
-    $("button#carouselbutton").click(function () {
-        $("._brexitstyle #Podcastslistboxone").removeClass("demo");
-        $("._brexitstyle #Podcastslistboxtwo").removeClass("demo");
-        $("._brexitstyle #Podcastslistboxthree").removeClass("demo");
-    });
-
-    $("._brexitstyle .carousel-container a.brexit-list").click(function () {
-        $(".podcast-recoder").show();
-        $("._brexitstyle .carousel-section").hide();
-    });
-
-    init = function () {
+  $("._brexitstyle .carousel-container a.brexit-list").click(function () {
+    var hiddenValue = $(this).find("input[type=hidden]:first").val();
+    if (hiddenValue != undefined && hiddenValue != null && hiddenValue != "") {
+      if (hiddenValue.includes("soundcloud.com")) {
+        // $(".podcastimagechild").find("iframe").attr("src", hiddenValue);
+        // $("._brexitstyle .carousel-section").hide();
+        // $(".podcast-recoder").show();
+        $(this).siblings(".podcastimages").children(".podcastimagechild").find("iframe").attr("src", hiddenValue);
+        $(this).parent(".podcast-recoder").siblings(".carousel-section").hide();
+        $(this).parent(".podcast-recoder").show();
+      } else {
+        window.open(hiddenValue);
+        // $(".podcast-recoder").hide();
+        // $("._brexitstyle .carousel-section").show();
+        $(this).parent(".podcast-recoder").hide();
+        $(this).parent(".podcast-recoder").siblings(".carousel-section").show();
+      }
+    } else {
+      window.open(hiddenValue);
+      // $(".podcast-recoder").hide();
+      // $("._brexitstyle .carousel-section").show();
+        $(this).parent(".podcast-recoder").hide();
+        $(this).parent(".podcast-recoder").siblings(".carousel-section").show();
     }
+  });
 
-    return {
-        init: init
-    }
-}(this, $INFORMA = jQuery.noConflict(), 'INFORMA'));
+  init = function () {};
+
+  return {
+    init: init,
+  };
+})(this, ($INFORMA = jQuery.noConflict()), "INFORMA");
 jQuery(INFORMA.articleTileView.init());
