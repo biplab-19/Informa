cb27ab1723155b8f14b881ae4fe760c2d0c2cab0
Rahul <jetsavvy.design02@outlook.com>
2020-07-23 17:22:24 +0530

    SW-9484

--- src/components/article-tile-view/js/article-tile-view.js
+++ src/components/article-tile-view/js/article-tile-view.js
@@ -1,18 +1,30 @@
 var INFORMA = window.INFORMA || {};
 INFORMA.articleTileView = (function (window, $, namespace) {
   "use strict";
-
   $(".carousel-container").slick({
     vertical: true,
     verticalSwiping: true,
     infinite: false,
     loop: false,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+     {
+        breakpoint: 1300,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      }
+    ]
+
   });
 
   $(".pc-body.Podcastslist .podcast-recoderbutton").click(function () {
    $(this).parent(".podcast-recoder").hide();
    $(this).parent(".podcast-recoder").siblings(".carousel-section").show();
-
+   $(this).parents(".carousel-section").show();
+   
   });
 
   $(".btn-Podcasts").click(function () {
@@ -23,36 +35,38 @@ INFORMA.articleTileView = (function (window, $, namespace) {
     $(this).closest(".demo").removeClass("demo");
   });
 
-  $("._brexitstyle .carousel-container a.brexit-list").click(function () {
-    var hiddenValue = $(this).find("input[type=hidden]:first").val();
-    if (hiddenValue != undefined && hiddenValue != null && hiddenValue != "") {
-      if (hiddenValue.includes("soundcloud.com")) {
-        // $(".podcastimagechild").find("iframe").attr("src", hiddenValue);
-        // $("._brexitstyle .carousel-section").hide();
-        // $(".podcast-recoder").show();
-        $(this).siblings(".podcastimages").children(".podcastimagechild").find("iframe").attr("src", hiddenValue);
-        $(this).parent(".podcast-recoder").siblings(".carousel-section").hide();
-        $(this).parent(".podcast-recoder").show();
+  $("._brexitstyle .carousel-container .brexit-list").click(function (ele) {
+    var hiddenValue = $(this).find("input[type=hidden]:first").val(),
+        hasbrexit = $(this).hasClass("brexit-list");
+        
+    if(hasbrexit) {
+      if (hiddenValue != undefined && hiddenValue != null && hiddenValue != "") {
+        if (hiddenValue.includes("soundcloud.com")) {
+            
+            $(this).parents(".carousel-section").siblings(".podcast-recoder").show();
+            $(this).parents(".carousel-section").siblings(".podcast-recoder").find("iframe").attr("src", hiddenValue);
+            $(this).parents(".carousel-section").siblings(".podcast-recoder").find(".brexit-list").empty();
+            $(this).parents(".carousel-section").siblings(".podcast-recoder").find(".brexit-list").append(ele.delegateTarget.innerHTML);
+            $(this).parents(".carousel-section").hide();
+          
+        } else {
+          window.open(hiddenValue);
+          $(this).parents(".carousel-section").siblings(".podcast-recoder").hide();
+          $(this).parents(".carousel-section").show();
+          
+          
+        }
       } else {
         window.open(hiddenValue);
-        // $(".podcast-recoder").hide();
-        // $("._brexitstyle .carousel-section").show();
-        $(this).parent(".podcast-recoder").hide();
-        $(this).parent(".podcast-recoder").siblings(".carousel-section").show();
+        $(this).parents(".carousel-section").siblings(".podcast-recoder").hide();
+        $(this).parents(".carousel-section").show();
+        
+       
+       
       }
-    } else {
-      window.open(hiddenValue);
-      // $(".podcast-recoder").hide();
-      // $("._brexitstyle .carousel-section").show();
-        $(this).parent(".podcast-recoder").hide();
-        $(this).parent(".podcast-recoder").siblings(".carousel-section").show();
     }
   });
 
-  init = function () {};
 
-  return {
-    init: init,
-  };
 })(this, ($INFORMA = jQuery.noConflict()), "INFORMA");
-jQuery(INFORMA.articleTileView.init());
+
