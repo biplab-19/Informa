e9c6bfc710fec7e32a63bc87378833a9d47aaf4c
Puneet Dhawan <puneet.dhawan@yendigital.com>
2020-07-28 00:02:31 +0530

    world chart data through json - SW-9483

--- src/components/world-chart/js/world-chart.js
+++ src/components/world-chart/js/world-chart.js
@@ -4,6 +4,8 @@ INFORMA.worldchart = (function(window, $, namespace) {
     //variables
     var init,
         Urls = INFORMA.Configs.urls.webservices,
+        _dataSourceId = $(".world-chart-background").data("source"),
+        _chartType = $(".world-chart-background").data("chart-type"),
         _getWorldChartData,
         _results,
         _createWorldChart,
@@ -12,22 +14,90 @@ INFORMA.worldchart = (function(window, $, namespace) {
         _worldChartBoxMove,
         _moveImage = 0;
 
-    //_getWorldChartData = function() {
-        //INFORMA.DataLoader.GetServiceData(Urls.PieBarChartPageData, {
-            //method: "GET",
-            //success_callback: function (data) {
-                //_results = data;
-                //_createWorldChart();
-            //},
-            //error_callback: function() {
+    _getWorldChartData = function() {
+        INFORMA.DataLoader.GetServiceData(Urls.WorldChartPageData +'?dataSourceId='+_dataSourceId + '&type='+_chartType, {
+            method: "GET",
+            success_callback: function (data) {
+                _results = data;
+                _createWorldChart();
+            },
+            error_callback: function() {
             
-            //}
-        //});
-    //}
+            }
+        });
+    }
 
     _createWorldChart = function() {
+        var starWithDots = [];
+        var facet = _results.ItemList;
+        var star = [{
+                "latitude": 50.8371,
+                "longitude": 4.3676,
+                "imageURL": "../../Static/images/star.svg",
+                "width": mobileZoom().Width,
+                "height": mobileZoom().Height,
+                "title": "2",
+                "balloonText": false
+            }, {
+                "latitude": 55.6763,
+                "longitude": 12.5681,
+                "imageURL": "../../Static/images/star.svg",
+                "width": mobileZoom().Width,
+                "height": mobileZoom().Height,
+                "title": "3",
+                "balloonText": false
+            }, {
+                "latitude": 40.712775,
+                "longitude": -74.005973,
+                "imageURL": "../../Static/images/star.svg",
+                "width": mobileZoom().Width,
+                "height": mobileZoom().Height,
+                "title": "4",
+                "balloonText": false
+            }, {
+                "latitude": 41.872389,
+                "longitude": 12.480180,
+                "imageURL": "../../Static/images/star.svg",
+                "width": mobileZoom().Width,
+                "height": mobileZoom().Height,
+                "title": "5",
+                "balloonText": false
+            }, {
+                "latitude": 51.507351,
+                "longitude": -0.127758,
+                "imageURL": "../../Static/images/star.svg",
+                "width": mobileZoom().Width,
+                "height": mobileZoom().Height,
+                "title": "6",
+                "balloonText": false
+            }, {
+                "latitude": 59.329323,
+                "longitude": 18.068581,
+                "imageURL": "../../Static/images/star.svg",
+                "width": mobileZoom().Width,
+                "height": mobileZoom().Height,
+                "title": "8",
+                "balloonText": false
+            }, 
+        ];
+        star.push(facet);
+        function flatten(e,starWithDots){
+            if(typeof e.length != "undefined")
+            {
+                for (var i=0;i<e.length;i++)
+                {
+                    flatten(e[i],starWithDots);
+                }
+            }
+            else
+            {
+                starWithDots.push(e);
+            }
+        }
+        flatten(star,starWithDots);
         _map = AmCharts.makeChart("inf-world-chart-wrapper", {
             "type": "map",
+            "hideCredits": "true",
             "projection": "miller",
 
             "dataProvider": {
@@ -133,101 +203,7 @@ INFORMA.worldchart = (function(window, $, namespace) {
                     "color": "#233e90",
                 }
                 ],
-                "images": [{
-                        "latitude": 48.856614,
-                        "longitude": 2.352222,
-                        "width": 16,
-                        "height": 16,
-                        "title": "France (20)",
-                        "scale": 0.5,
-                        "type": "circle",
-                        "color": "#ed2024",
-                    }, {
-                        "latitude": 50.8371,
-                        "longitude": 4.3676,
-                        "imageURL": "../../Static/images/star.svg",
-                        "width": mobileZoom().Width,
-                        "height": mobileZoom().Height,
-                        "title": "2",
-                        "balloonText": false
-                    }, {
-                        "latitude": 55.6763,
-                        "longitude": 12.5681,
-                        "imageURL": "../../Static/images/star.svg",
-                        "width": mobileZoom().Width,
-                        "height": mobileZoom().Height,
-                        "title": "3",
-                        "balloonText": false
-                    }, {
-                        "latitude": 40.712775,
-                        "longitude": -74.005973,
-                        "imageURL": "../../Static/images/star.svg",
-                        "width": mobileZoom().Width,
-                        "height": mobileZoom().Height,
-                        "title": "4",
-                        "balloonText": false
-                    }, {
-                        "latitude": 41.872389,
-                        "longitude": 12.480180,
-                        "imageURL": "../../Static/images/star.svg",
-                        "width": mobileZoom().Width,
-                        "height": mobileZoom().Height,
-                        "title": "5",
-                        "balloonText": false
-                    }, {
-                        "latitude": 51.507351,
-                        "longitude": -0.127758,
-                        "imageURL": "../../Static/images/star.svg",
-                        "width": mobileZoom().Width,
-                        "height": mobileZoom().Height,
-                        "title": "6",
-                        "balloonText": false
-                    }, {
-                        "latitude": 59.329323,
-                        "longitude": 18.068581,
-                        "imageURL": "../../Static/images/star.svg",
-                        "width": mobileZoom().Width,
-                        "height": mobileZoom().Height,
-                        "title": "8",
-                        "balloonText": false
-                    }, {
-                        "latitude": 28.6139,
-                        "longitude": 77.2090,
-                        "width": 16,
-                        "height": 16,
-                        "scale": 0.5,
-                        "type": "circle",
-                        "color": "#ed2024",
-                        "title": "India (5)"
-                    }, {
-                        "latitude": -25.2744,
-                        "longitude": 133.7751,
-                        "width": 16,
-                        "height": 16,
-                        "scale": 0.5,
-                        "type": "circle",
-                        "color": "#ed2024",
-                        "title": "Australia (39)"
-                    }, {
-                        "latitude": 56.1304,
-                        "longitude": 106.3468,
-                        "width": 16,
-                        "height": 16,
-                        "scale": 0.5,
-                        "type": "circle",
-                        "color": "#ed2024",
-                        "title": "Russia (22)"
-                    }, {
-                        "latitude": 37.0902,
-                        "longitude": -95.7129,
-                        "width": 16,
-                        "height": 16,
-                        "scale": 0.5,
-                        "type": "circle",
-                        "color": "#ed2024",
-                        "title": "United States (84)"
-                    },
-                ],
+                "images": starWithDots,
             },
 
             "responsive": {
@@ -331,7 +307,7 @@ INFORMA.worldchart = (function(window, $, namespace) {
     }
 
     init = function() {
-        _createWorldChart();
+        _getWorldChartData();
         _worldChartBoxExpand();
         _worldChartBoxMove();
     };
