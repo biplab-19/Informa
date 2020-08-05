f18d2a2fbe790488983e09e19a85f4eb2aa20ffe
Puneet Dhawan <puneet.dhawan@yendigital.com>
2020-08-05 21:42:07 +0530

    added scroll down image - SW-9485

---
+++ src/components/campaign-background/js/campaign-background.js
@@ -0,0 +1,15 @@
+var INFORMA = window.INFORMA || {};
+INFORMA.campaignbackground = (function(window, $, namespace) {
+    'use strict';
+    //variables
+    var init
+
+    init = function() {
+        
+    };
+
+    return {
+        init: init
+    };
+}(this, jQuery, 'INFORMA'));
+jQuery(INFORMA.campaignbackground.init());
