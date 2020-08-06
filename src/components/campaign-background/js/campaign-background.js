3fa79d42c7567428603d362f20000346623bae27
Rahul <jetsavvy.design02@outlook.com>
2020-08-06 16:58:01 +0530

    SW-9485 scroll down js code

--- src/components/campaign-background/js/campaign-background.js
+++ src/components/campaign-background/js/campaign-background.js
@@ -1,11 +1,36 @@
 var INFORMA = window.INFORMA || {};
-INFORMA.campaignbackground = (function(window, $, namespace) {
+INFORMA.campaignbackground = (function (window, $, namespace) {
     'use strict';
     //variables
-    var init
+    var init,
+        scrollImg = $("img.scroll-down").length,
+        docHeight = $("body").height(),
+        headerHeight = $("#informa-main-header").height(),
+        footerHeight = $("#informa-footer").height(),
+        cookiesHeight = $("#cookieBanner").height(),
+        scrollhideHeight = parseInt(docHeight) - (parseInt(headerHeight) + parseInt(footerHeight) + parseInt(cookiesHeight) + 300),
+        scrollfromTop,
+        //methods
+        scrollHide,
+        scrollHide = function (scrollfromTop) {
+            $(".scroll-down").removeClass("hide");
+            if (scrollImg > 0) {
+                if (scrollfromTop >= scrollhideHeight) {
+                    $(".scroll-down").addClass("hide");
+                }
+                else {
+                    $(".scroll-down").removeClass("hide");
+                }
+            }
+        }
+    init = function () {
 
-    init = function() {
-        
+        window.addEventListener("scroll", function (event) {
+            var scroll = this.scrollY;
+            scrollHide(scroll);
+        });
+
+
     };
 
     return {
