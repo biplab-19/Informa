7038b8c224cb5f3d73e2179b366035cd83d54d77
Rahul <jetsavvy.design02@outlook.com>
2020-08-07 17:54:23 +0530

    SW-9485

--- src/components/campaign-background/js/campaign-background.js
+++ src/components/campaign-background/js/campaign-background.js
@@ -3,19 +3,23 @@ INFORMA.campaignbackground = (function (window, $, namespace) {
     'use strict';
     //variables
     var init,
-        _scrollDown = $(".scroll-down"),
-        _scrollImg = $("img.scroll-down").length,
-        _docHeight = $("body").height(),
-        _headerHeight = $("#informa-main-header").height(),
-        _footerHeight = $("#informa-footer").height(),
-        _cookiesHeight = $("#cookieBanner").height(),
-        _scrollhideHeight = parseInt(_docHeight) - (parseInt(_headerHeight) + parseInt(_footerHeight) + parseInt(_cookiesHeight) + 1300),
-        _scrollfromTop,
+    _scrollfromTop,
         //methods
         _scrollHide
 
     _scrollHide = function (_scrollfromTop) {
-        _scrollDown.removeClass("hide");
+        var _docHeight = $("body").height(),
+            _scrollDown = $(".scroll-down"),
+            _scrollImg = $("img.scroll-down").length,
+            _docHeight = $("body").height(),
+            _headerHeight = $("#informa-main-header").height(),
+            _footerHeight = $("footer").height(),
+            _cookiesHeight = $("#cookieBanner").height();
+            if(_cookiesHeight == null || _cookiesHeight == undefined){
+                _cookiesHeight = 170;
+            }
+           var  _scrollhideHeight = parseInt(_docHeight) - (parseInt(_headerHeight) + parseInt(_footerHeight) + parseInt(_cookiesHeight) + 300);
+            _scrollDown.removeClass("hide");
         if (_scrollImg > 0) {
             if (_scrollfromTop >= _scrollhideHeight) {
                 _scrollDown.addClass("hide");
